dc568d0f34a3eefd6d63e0a34e4a27cce7b9b3a0
J Scott Chapman <scott8675309@gmail.com>
2016-05-13 08:49:57 -0600

    Initial Commit
    Included an example of the Semantic-UI Ember shape on the information
    page for an interesting way to potentially present options.

---
+++ app/controllers/signup/information.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  stateOptions: ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'],
+  selectedState: null,
+  useMiddleName: false,
+  agreeTerms: false,
+
+  actions: {
+    updateSelected: function(component, id, value) {
+      this.set('selectedState', id);
+    },
+
+    flip: function(direction) {
+      Ember.$('.ui.shape').shape('flip ' + direction);
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('vogue');
+  this.route('signup', function() {
+    this.route('billing');
+    this.route('information');
+  });
 });
 
 export default Router;
