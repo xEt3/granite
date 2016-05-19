56187980fadd8b43f1d4c679994bf3dacf736cb1
J Scott Chapman <scott8675309@gmail.com>
2016-05-19 13:23:10 -0600

    Created "sent email" page.  Closes #14.

---
+++ tests/unit/routes/setup/set-password-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:setup/set-password', 'Unit | Route | setup/set password', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
