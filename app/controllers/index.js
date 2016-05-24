48a40529bde42e9b1db63ceecf5badb2c2e0158d
James-Mac <james@aehr.org>
2016-05-24 10:25:25 -0600

    Refactoring a bit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { on, run, $, set } = Ember;
+const { on, run, $ } = Ember;
 
 export default Ember.Controller.extend({
   getWindowHeight: on('init', function () {
