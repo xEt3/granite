ed880a1e90cdde3274e9688ebabb66cecedc620d
James-Mac <james@aehr.org>
2016-05-27 13:17:21 -0600

    Create actual action

--- app/controllers/signup/billing.js
+++ app/controllers/signup/billing.js
@@ -5,8 +5,21 @@ const { Controller } = Ember;
 
 export default Controller.extend(ajaxStatus, {
   actions: {
-    createPaymentMethod ( nonce ) {
+    submit ( nonce ) {
+      const model = this.get('model');
+
       this.ajaxStart();
+
+      model.save()
+      .then(company => {
+        var paymentMethod = this.store.createRecord('payment-method', { nonce, company });
+        return paymentMethod.save();
+      })
+      .then(() => {
+        this.ajaxSuccess();
+        this.transitionToRoute('signup.finish');
+      })
+      .catch(this.ajaxError.bind(this));
     }
   }
 });
