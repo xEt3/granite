9d3b4f2ced10c1a5a47033db94c1aace87da0124
James-Mac <james@aehr.org>
2016-06-08 08:24:49 -0600

    Working on fixing semantic

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,20 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
+const { Controller, computed, observer, on, run, $ } = Ember;
 
-export default Ember.Controller.extend({
+export default Controller.extend({
   navTransparent: computed.equal('currentPath', 'index'),
+
   topLevel: computed('currentPath', function () {
     return this.get('currentPath').indexOf('account') < 0;
-  })
+  }),
+
+  updateBodyClass: on('init', observer('topLevel', function () {
+    console.log('test')
+    run.next(() => {
+      console.log('topLevel changed');
+      console.log((this.get('topLevel') ? 'removeClass' : 'addClass'), 'application__in-account on body');
+      $('body')[this.get('topLevel') ? 'removeClass' : 'addClass']('application__in-account');
+    });
+  }))
 });
