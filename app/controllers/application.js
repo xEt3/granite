ce844753ee9c8be3355b0d0c5ccd9e95200ada0a
James-Mac <james@aehr.org>
2016-06-10 10:22:16 -0600

    Build account nav from array, dynamic breadcrumbs, and stylizations

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,32 @@ import Ember from 'ember';
 const { Controller, computed, observer, on, run, $ } = Ember;
 
 export default Controller.extend({
+  accountNavigationItems: [{
+    icon: 'dashboard',
+    title: 'Dashboard',
+    link: 'index'
+  }, {
+    icon: 'check',
+    title: 'Action Items',
+    link: 'action-items'
+  }, {
+    icon: 'users',
+    title: 'Employees',
+    link: 'employees'
+  }, {
+    icon: 'user add',
+    title: 'Recruiting',
+    link: 'recruiting'
+  }, {
+    icon: 'file',
+    title: 'Documents',
+    link: 'documents'
+  }, {
+    icon: 'doctor',
+    title: 'Leave Management',
+    link: 'index'
+  }],
+
   navTransparent: computed.equal('currentPath', 'index'),
 
   topLevel: computed('currentPath', function () {
