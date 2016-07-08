c6ee18298680ec05267a7d0c7d27a046102728f9
J Scott Chapman <scott8675309@gmail.com>
2016-07-08 12:35:33 -0600

    Intermediate commit

--- app/controllers/account/recruiting/new.js
+++ app/controllers/account/recruiting/new.js
@@ -4,4 +4,6 @@ import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 const { Controller } = Ember;
 
 export default Controller.extend(addEdit, {
+  transitionWithModel: true,
+  transitionAfterSave: 'account.recruiting.job-description.settings'
 });
