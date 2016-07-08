de1208f720a4c4d81722d214f9c4b4959ca9c3d8
James-Mac <james@aehr.org>
2016-07-08 12:58:37 -0600

    Fix it more

--- app/views/application.js
+++ app/views/application.js
@@ -6,6 +6,9 @@ export default Component.extend({
   classNames: [ 'pusher', 'granite-application-container' ],
 
   moveApplicationTag: on('didInsertElement', function () {
-    run.scheduleOnce('afterRender', () => this.$().insertBefore('.ember-view:first'));
+    run.scheduleOnce('afterRender', () => {
+      this.$().insertBefore('.ember-view:first');
+      this.$('body > .sidebar.ember-view').removeClass('ember-view');
+    });
   })
 });
