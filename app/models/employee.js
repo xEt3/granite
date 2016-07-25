eb591248bbe0638d95bbf4c87c81f537a32bb809
J Scott Chapman <scott8675309@gmail.com>
2016-07-25 14:06:35 -0600

    Added employee model and created guessing table.

---
+++ app/models/employee.js
@@ -0,0 +1,20 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  firstName:  attr('string'),
+  middleName: attr('string'),
+  lastName:   attr('string'),
+  suffixName: attr('string'),
+
+  ssn:      attr('string'),
+  phone:    attr('string'),
+  email:    attr('string'),
+
+  created: attr('date', {
+    defaultValue () {
+      return new Date();
+    }
+  })
+});
