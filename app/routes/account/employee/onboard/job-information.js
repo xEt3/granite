3b93379ccb59c7aaf9010b2a80adb7111b312d74
J Scott Chapman <scott8675309@gmail.com>
2016-08-02 07:42:18 -0600

    Started job-information page

--- app/routes/account/employee/onboard/job-information.js
+++ app/routes/account/employee/onboard/job-information.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(addEdit,{
 });
