98bab2d4816966e091373c4ba69b325e4ccc4d66
J Scott Chapman <scott8675309@gmail.com>
2016-08-03 10:56:57 -0600

    Added some fields to job-information

---
+++ tests/unit/controllers/account/employee/onboard/job-information-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:account/employee/onboard/job-information', 'Unit | Controller | account/employee/onboard/job information', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
