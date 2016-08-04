6480a4ec89c185f2b8ffa9912485de541c03a76e
James-Mac <james@aehr.org>
2016-08-04 11:45:41 -0600

    Add asset management pages

--- app/routes/account/assets/index.js
+++ app/routes/account/assets/index.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route } = Ember;
+
+export default Route.extend({
+  queryParams: {
+    page: { refreshModel: true }
+  },
+
+  model ( params ) {
+    let limit = this.get('controller.limit') || 20,
+        page = (params.page || 1) - 1;
+
+    return this.store.query('asset', {
+      page,
+      limit
+    });
+  }
 });
