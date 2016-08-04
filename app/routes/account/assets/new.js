46a10d19d5285c443d0d4130a54d19606daa57da
J Scott Chapman <scott8675309@gmail.com>
2016-08-04 13:15:51 -0600

    Intermediate commit

--- app/routes/account/assets/new.js
+++ app/routes/account/assets/new.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+import add from 'granite/mixins/route-abstractions/add';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(add, {
+  auth: Ember.inject.service(),
+  modelName: 'asset',
+
+  getModelDefaults () {
+    return {
+      creator: this.get('auth.user'),
+      company: this.get('auth.user.company')
+    };
+  }
 });
