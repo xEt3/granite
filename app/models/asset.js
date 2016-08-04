e6b9c7debc7be1820960fe1919659ab8a1a5e850
J Scott Chapman <scott8675309@gmail.com>
2016-08-04 10:44:43 -0600

    Added models regarding assets.
    Added models for:
    -asset
    -asset-item
    -asset-assignment

---
+++ app/models/asset.js
@@ -0,0 +1,18 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  name:        attr('string'),
+  description: attr('string'),
+  sharable:    attr('boolean'),
+
+  creator:     belongsTo('company-user'),
+  company:     belongsTo('company'),
+
+  created: attr('date', {
+    defaultValue () {
+      return new Date();
+    }
+  })
+});
