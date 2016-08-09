1b3d3aa5961d9f6aa855fe78fba4de25fe4a2c35
James-Mac <james@aehr.org>
2016-08-09 12:59:38 -0600

    Work on asset categories and confirm-modal features

--- app/routes/account/asset/index.js
+++ app/routes/account/asset/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route } = Ember;
+
+export default Route.extend({
+  model () {
+    return this.store.query('asset-item', {
+      asset: this.paramsFor('account.asset').id
+    });
+  }
 });
---
+++ tests/unit/controllers/account/asset/index/new-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:account/asset/index/new', 'Unit | Controller | account/asset/index/new', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
