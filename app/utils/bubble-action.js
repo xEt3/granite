7b413f2c1ef4636487082b97c095e5393146eb8d
James-Mac <james@aehr.org>
2016-08-11 12:56:21 -0600

    Add doc to bubble-action

--- app/utils/bubble-action.js
+++ app/utils/bubble-action.js
@@ -3,3 +3,35 @@ export default function bubbleAction ( proxyTo ) {
     this.get(proxyTo).apply(this, arguments);
   };
 }
+
+/*
+  Usage:
+
+  /components/some-test
+  ```javascript
+  import bubblesTo from 'granite/utils/bubble-action';
+
+  Ember.Component.extend({
+  ...
+    actions: {
+      saveIt: bubblesTo('mySaveAction')
+    }
+  });
+  ```
+
+  /templates/my-page
+
+  ```html
+  {{some-test mySaveAction=(action 'save')}}
+  ```
+
+  /controllers/my-page
+  ```html
+  ...
+  actions: {
+    save ( args, from, action, helper ) {
+      // do something
+    }
+  }
+  ```
+ */
