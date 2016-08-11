f1e2b5de801e93d62c60abcafcfdc695871a75b8
James-Mac <james@aehr.org>
2016-08-11 07:39:43 -0600

    Onboard/equipment work, auth tweak

--- app/routes/account/employee/onboard/equipment.js
+++ app/routes/account/employee/onboard/equipment.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, RSVP } = Ember;
+
+export default Route.extend({
+  model () {
+    let employee = this.modelFor('account.employee.onboard');
+
+    return RSVP.hash({
+      employee,
+      assignableAssets: this.store.findAll('asset').then(assets => {
+        return RSVP.map(assets.toArray(), asset => {
+          let itemQuery = { asset: asset.get('id') };
+
+          if ( !asset.get('sharable') ) {
+            itemQuery['assignments.0'] = { $exists: false };
+          }
+
+          return this.store.query('asset-item', itemQuery)
+          .then(stock => Ember.Object.create({ asset, stock }));
+        });
+      }),
+
+      assignedAssets: this.store.query('asset-item', {
+        'assignments.employee': employee.get('id')
+      })
+    });
+  },
+
+  setupController ( controller, model ) {
+    controller.setProperties({
+      model: model.employee,
+      assignableAssets: model.assignableAssets,
+      assignedAssets: model.assignedAssets
+    });
+  }
 });
