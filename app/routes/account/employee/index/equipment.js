11e2f3d9cbbe468269e1409b37d27cf07ced0693
J Scott Chapman <scott8675309@gmail.com>
2016-08-15 11:30:55 -0600

    Assigned Asset Item Component.  Closes #44.

--- app/routes/account/employee/index/equipment.js
+++ app/routes/account/employee/index/equipment.js
@@ -1,8 +1,20 @@
 import Ember from 'ember';
 
+const { RSVP } = Ember;
+
 export default Ember.Route.extend({
   model () {
-    return this.store.query('asset-item', {
+    let employee = this.modelFor('account.employee');
+    return RSVP.hash({
+      assetItems: this.store.query('asset-item', { 'assignments.employee': employee.get('id') }),
+      employee
+    });
+
+  },
+  setupController ( controller, model ) {
+    controller.setProperties({
+      model: model.assetItems,
+      employee: model.employee
     });
   }
 });
