60f20c58b0acb067270e59e371f6bb5754bec1a4
James-Mac <james@aehr.org>
2016-08-15 11:58:07 -0600

    Update ember-ajax, use ember-ajax in ember data adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import AjaxServiceSupport from 'ember-ajax/mixins/ajax-support';
 
-export default DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend(AjaxServiceSupport, {
   namespace: 'api/v1',
   pathForType ( type ) {
     return Ember.String.pluralize(type);
