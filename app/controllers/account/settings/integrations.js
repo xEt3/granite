1871b4c1c54215363266041aa123c6fe0fbf3ded
James-Mac <james@aehr.org>
2016-09-02 13:24:17 -0600

    Finish up slate payroll integration linkage

--- app/controllers/account/settings/integrations.js
+++ app/controllers/account/settings/integrations.js
@@ -1,11 +1,30 @@
 import Ember from 'ember';
+import ajaxStatus from 'granite/mixins/ajax-status';
 
-const { Controller } = Ember;
+const { Controller, inject } = Ember;
 
-export default Controller.extend({
+export default Controller.extend(ajaxStatus, {
+  ajax: inject.service(),
+
   actions: {
     onNotify () {
       this.send.apply(this, [ 'notify', ...arguments ]);
+    },
+
+    grant ( data ) {
+      this.ajaxStart();
+
+      this.get('ajax').request('/api/v1/integrations/grant/' + data.id, {
+        method: 'POST',
+        data: {
+          grant: data.grant
+        }
+      })
+      .then(() => {
+        this.ajaxSuccess('Successfully linked service.');
+        this.send('refresh');
+      })
+      .catch(this.ajaxError.bind(this));
     }
   }
 });
