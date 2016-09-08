0ec279a7af269c4cf888636f5dc950e8c8e77a25
J Scott Chapman <scott8675309@gmail.com>
2016-09-08 15:08:46 -0600

    Initial commit department & location.  Closes #56 and #55

--- app/models/location.js
+++ app/models/location.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
+
+  name: attr('string'),
+  code: attr('string'),
+
+  company: belongsTo('company'),
+
+  created: attr('date')
 
 });
