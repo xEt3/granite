c6b0956facc7797735fb7a4568b2437d06867e95
James-Mac <james@aehr.org>
2016-09-08 06:55:43 -0600

    Spec out activity model with actor/target resolution

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,8 +4,14 @@ import ajaxStatus from 'granite/mixins/ajax-status';
 const { Controller } = Ember;
 
 export default Controller.extend(ajaxStatus, {
+  activity: Ember.inject.service(),
   actions: {
     login () {
+      let a = this.store.createRecord('activity', {
+        icon: 'test'
+      });
+      console.log(a);
+      console.log(a.get('test'));
       const email = this.get('email'),
             password = this.get('password');
 
@@ -16,6 +22,12 @@ export default Controller.extend(ajaxStatus, {
       }
 
       this.auth.login(email, password)
+      .then(() => {
+        return this.get('activity').create({
+          description: '{{actor.doc.name.first}} logged in for the first time. Good for you buddy.',
+          icon: 'check'
+        });
+      })
       .then(() => {
         let previousTransition = this.get('previousTransition');
         this.ajaxSuccess('Successfully logged in.');
--- app/models/activity.js
+++ app/models/activity.js
@@ -1,19 +1,45 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 
+const { computed } = Ember;
+
 export default Model.extend({
-  actor:   belongsTo(),
+  description: attr('string'),
   action:  attr('string'),
-  target:  belongsTo(),
   icon:    attr('string'),
 
-  company: belongsTo('company'),
+  actorId: attr('string'),
+  actorType: attr('string'),
+  targetId: attr('string'),
+  targetType: attr('string'),
 
+  company: belongsTo('company'),
 
   created: attr('date', {
     defaultValue () {
       return new Date();
     }
+  }),
+
+  actor: computed('actorType', 'actorId', function () {
+    const type = this.get('actorType'),
+          // TODO ^ type needs to be munged before we use it.
+          // Right now it will come in as 'Model' or 'ModelTest' camel casing
+          // we need lowercase dasherized to resolve it ('model' or 'model-test')
+          id = this.get('actorId');
+
+    return type && id ? this.store.find(type, id) : undefined;
+  }),
+
+  target: computed('targetType', 'targetId', function () {
+    const type = this.get('targetType'),
+          // TODO ^ type needs to be munged before we use it.
+          // Right now it will come in as 'Model' or 'ModelTest' camel casing
+          // we need lowercase dasherized to resolve it ('model' or 'model-test')
+          id = this.get('targetId');
+
+    return type && id ? this.store.find(type, id) : undefined;
   })
 });
