0ac77180ca2b5e1de72573e705c288b1533394b2
J Scott Chapman <scott8675309@gmail.com>
2016-09-12 07:47:34 -0600

    Pre-working condition

--- app/controllers/account/employee/onboard/job-information.js
+++ app/controllers/account/employee/onboard/job-information.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['employee']
+  // locations: function() {
+  //   return this.get('company').locations;
+  // },
+  // departments: function() {
+  //   return this.get('company').departments;
+  // }
 });
--- app/routes/account/employee/onboard/job-information.js
+++ app/routes/account/employee/onboard/job-information.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 
+const { inject } = Ember;
+
 export default Ember.Route.extend(addEdit,{
+  queryParams: {
+    employee: {
+      refreshModel: false
+    }
+  },
+
+  auth: inject.service(),
+
+  model() {
+    return this.get('auth.user.company');
+  }
+
+
 });
