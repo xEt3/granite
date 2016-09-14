f8149ca8939ab6886ccc3252d3b680d5b680a059
J Scott Chapman <scott8675309@gmail.com>
2016-09-14 15:26:59 -0600

    Some more changes.
    Still need to tweak the account.anatomy.locations.hbs and the
    account.anatomy.departments.hbs so that they look right.

--- app/controllers/account/anatomy/locations/index/new.js
+++ app/controllers/account/anatomy/locations/index/new.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
+import { states } from 'granite/config/statics';
 import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 
-export default Ember.Controller.extend(addEdit, {
+const { Controller, computed } = Ember;
+
+export default Controller.extend(addEdit, {
+  states,
+  stateIsMontana: computed.equal('model.addressState', 'MT'),
   transitionAfterSave: 'account.anatomy.locations.index',
-  transitionWithModel: false
+  transitionWithModel: false,
+
+  afterSave () {
+    this.send('refresh');
+  }
 });
--- app/models/location.js
+++ app/models/location.js
@@ -4,10 +4,18 @@ import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
 
-  name: attr('string'),
-  code: attr('string'),
+  name:  attr('string'),
+  code:  attr('string'),
+  phone: attr('string'),
+
+  addressLine1:              attr('string'),
+  addressLine2:              attr('string'),
+  addressCity:               attr('string'),
+  addressState:              attr('string'),
+  addressZipcode:                attr('string'),
 
   company: belongsTo('company'),
+  creator: belongsTo('company-user', { async: true, inverse: false }),
 
   created: attr('date', {
     defaultValue () {
