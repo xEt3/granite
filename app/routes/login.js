ef67c7c58d9bb36f2eb7df31d97a5931726e7601
James-Mac <james@aehr.org>
2016-09-19 12:35:16 -0600

    Things

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 const { Route } = Ember;
 
 export default Route.extend({
+  resetController ( controller, isExiting ) {
+    if ( isExiting ) {
+      controller.set('expired', false);
+    }
+  },
+
   actions: {
     willTransition () {
       this.get('controller').set('previousTransition', null);
