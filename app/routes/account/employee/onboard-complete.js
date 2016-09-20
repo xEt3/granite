280eb47886adfdef327292aca72421b14ea7b844
J Scott Chapman <scott8675309@gmail.com>
2016-09-20 09:22:28 -0600

    Added info and transition for onboarding.  Closes #49.

--- app/routes/account/employee/onboard-complete.js
+++ app/routes/account/employee/onboard-complete.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const { Route, run } = Ember;
 
 export default Route.extend({
+  afterModel (model) {
+    model.setProperties({
+      onboarding: false,
+      onboardingStep: null,
+      onboardingProgress: null
+    });
+    run.scheduleOnce('afterRender', () => {
+      run.later(this, () => {
+        this.transitionTo('account.employees.index');
+      }, 3000);
+    });
+  }
 });
