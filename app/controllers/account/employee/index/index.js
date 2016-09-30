a9f324982b20673fa87298ea0ca4a525b99a9673
J Scott Chapman <scott8675309@gmail.com>
2016-09-30 09:35:18 -0600

    Employee page changes. Closes #80.
    Styled contact buttons.
    Created the ability to select no supervisor, no department, and no
    location.
    Made the page tolerant of very little information being entered for the
    employee.

--- app/controllers/account/employee/index/index.js
+++ app/controllers/account/employee/index/index.js
@@ -2,7 +2,18 @@ import Ember from 'ember';
 const { computed, Controller } = Ember;
 export default Controller.extend({
 
-  hasEmergencyContact: computed.notEmpty(
+
+  hasAnniversaries: computed.or(
+    'model.hireDate',
+    'model.dateOfBirth'
+  ),
+
+  hasDirectContact: computed.or(
+    'model.phone',
+    'model.email'
+  ),
+
+  hasEmergencyContact: computed.or(
     'model.emergencyContactPhone',
     'model.emergencyContactNameFirst',
     'model.emergencyContactNameLast'
