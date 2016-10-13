5c40c5abb355f95f75a7688aa7f7d864776d031c
James-Mac <james@aehr.org>
2016-10-13 13:33:30 -0600

    Improve addEdit abstraction, fix transparent inputs, add custom field component

---
+++ app/components/tables/custom-field.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+const { Component } = Ember;
+
+export default Component.extend({
+  tagName: 'tr',
+
+  actions: {
+    editValue () {
+      this.setProperties({
+        editingValue: true,
+        newValue: this.get('value')
+      });
+    },
+
+    cancelEdit () {
+      this.set('editingValue', false);
+    },
+
+    saveEdit () {
+      this.get('onValueChange')(this.get('key'), this.get('newValue'));
+      this.set('editingValue', false);
+    },
+
+    delete () {
+      this.get('onDelete')(this.get('key'));
+    }
+  }
+});
