277d01b5f8bbcfb91509cf20a128d245b0e8583e
J Scott Chapman <scott8675309@gmail.com>
2016-10-13 08:39:20 -0600

    Custom Fields Initial Commit

--- app/controllers/account/employee/onboard/custom-fields.js
+++ app/controllers/account/employee/onboard/custom-fields.js
@@ -6,16 +6,18 @@ const { Controller, computed } = Ember;
 
 export default Controller.extend(addEdit, del, {
   addingCustomFieldName: false,
+  addingCustomFieldValue: false,
+  adding: computed.or('addingCustomFieldName', 'addingCustomFieldValue'),
 
   customFieldHelper: computed('addingCustomFieldName', function () {
     return this.get('addingCustomFieldName') ? 'Cancel' : 'Add a new custom field';
   }),
 
   afterSave () {
-    this.setProperties({
-      pendingCustomFieldName: null,
-      addingCustomFieldName: false
-    });
+    // this.setProperties({
+    //   pendingCustomFieldName: null,
+    //   addingCustomFieldName: false
+    // });
   },
 
   actions: {
@@ -23,22 +25,71 @@ export default Controller.extend(addEdit, del, {
       this.toggleProperty(prop);
     },
 
-    saveCustomFieldName () {
+    createCustomField () {
+      Ember.$('.new-custom-field').modal('show');
+    },
+    saveCustomField () {
       let model = this.get('model'),
-          attr = this.get('pendingCustomFieldName');
+          attr = this.get('pendingCustomFieldName'),
+          value = this.get('pendingCustomFieldValue');
 
       this.ajaxStart();
 
       if ( !attr ) {
-        this.ajaxError('Custom Field name is required.');
+        this.ajaxError('Custom field name is required.');
+        return;
+      } else if ( !value ) {
+        this.ajaxError('Custom field value is required.');
         return;
       }
+
 
       if ( !model.get('customFields') ) {
         model.set('customFields', {});
       }
-      model.set(`customFields.${attr}`, null);
+      model.set(`customFields.${attr}`, value);
       this.send('save', model);
+      this.setProperties({
+        addingCustomFieldValue: true
+      });
+    },
+
+    abortCustomField() {
+
     }
+
   }
 });
+
+// actions: {
+//   createAsset ( category ) {
+//     let user = this.get('auth.user');
+//
+//     this.set('pendingAssetItem', this.store.createRecord('asset-item', {
+//       asset: category,
+//       identifier: this.get('model.firstName') + '\'s ' + singularize(category.get('name')),
+//       creator: user,
+//       company: user.get('company')
+//     }));
+//
+//     Ember.$('.new-asset').modal('show');
+//   },
+//
+//   saveAsset () {
+//     this.ajaxStart();
+//
+//     this.get('pendingAssetItem').save()
+//     .then(asset => {
+//       this.send('selectAsset', asset);
+//     })
+//     .catch(this.ajaxError.bind(this));
+//   },
+//
+//   abortAsset () {
+//     let asset = this.get('pendingAssetItem');
+//
+//     asset.destroyRecord()
+//     .then(() => {
+//       this.set('pendingAssetItem', null);
+//     });
+//   },
