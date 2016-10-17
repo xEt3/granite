f2574981a0e282831d0ce0e2513fb7a67fca8957
James-Mac <james@aehr.org>
2016-10-17 14:33:24 -0600

    IMHO

--- app/routes/account/action-items/new.js
+++ app/routes/account/action-items/new.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
+import add from 'granite/mixins/route-abstractions/add';
 
-export default Ember.Route.extend({
+const { Route, RSVP, inject } = Ember;
+
+export default Route.extend(add, {
+  auth: inject.service(),
+  modelName: 'action-item',
+
+  getModelDefaults () {
+    return {
+      owner: this.get('auth.user'),
+      company: this.get('auth.user.company')
+    };
+  },
+
+  model () {
+    return RSVP.hash({
+      actionItem: {},//this._super(...arguments),
+      actionItems: this.store.query('action-item', {
+        completedOn: { $ne: { $type: 9 } },
+        cancelledOn: { $ne: { $type: 9 } }
+      }),
+      users: this.store.findAll('company-user')
+    });
+  }
 });
