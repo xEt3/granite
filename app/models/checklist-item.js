e27cb41f206f50a3dd912b7a0b797875b30e1be5
J Scott Chapman <scott8675309@gmail.com>
2016-10-17 14:34:34 -0600

    Intermediate Changes.

---
+++ app/models/checklist-item.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+import Model from 'ember-data/model';
+const { attr, belongsTo } = DS;
+
+export default Model.extend({
+  completedOn: attr('date'),
+  assignedOn: attr('date'),
+  priority: attr('number'),
+  assignedTo: belongsTo('company-user', { async: true }),
+  assignedBy: belongsTo('company-user', { async: true }),
+  completedBy: belongsTo('company-user', { async: true })
+});
