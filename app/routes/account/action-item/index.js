50aae5a69e11f495628736292530b5a9ebe5f05a
J Scott Chapman <scott8675309@gmail.com>
2016-10-17 15:22:46 -0600

    Floobits
    :trumpet:

--- app/routes/account/action-item/index.js
+++ app/routes/account/action-item/index.js
@@ -5,7 +5,22 @@ const { Route } = Ember;
 export default Route.extend({
   model () {
     return {
-      title: 'Developer Recruiting Project'
-    };
+      title: 'Developer Recruiting Project',
+      target: 'Sam Harris',
+      created: moment('2016/09/10'),
+      description: 'Project to find a UX designer for the Granite Project since Exxon has decided to adopt Granite HR exclusively Lolz : )',
+      dueOn: moment('2016/11/16'),
+      prerequisites: [
+      { title: 'Write Job Description for UX Designer',
+        checklist: [
+          {title: 'Prepare Final Draft'},
+          {title: 'Approve Final Draft'}
+        ]
+      }  
+      ]
+
+
+
+    }
   }
 });
