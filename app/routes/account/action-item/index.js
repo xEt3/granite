b44311e15ed901faae68c8684664324d018c40aa
J Scott Chapman <scott8675309@gmail.com>
2016-10-17 15:34:18 -0600

    Hooking it up(start)

--- app/routes/account/action-item/index.js
+++ app/routes/account/action-item/index.js
@@ -11,16 +11,26 @@ export default Route.extend({
       description: 'Project to find a UX designer for the Granite Project since Exxon has decided to adopt Granite HR exclusively Lolz : )',
       dueOn: moment('2016/11/16'),
       prerequisites: [
-      { title: 'Write Job Description for UX Designer',
-        checklist: [
-          {title: 'Prepare Final Draft'},
-          {title: 'Approve Final Draft'}
-        ]
-      }  
+        { title: 'Write Job Description for UX Designer',
+          checklist: [
+            {title: 'Prepare Final Draft'},
+            {title: 'Approve Final Draft'}
+          ]
+        },
+        { title: 'Perform Salary Survey for 2016',
+          checklist: [
+            {title: 'Gather Sources'},
+            {title: 'Compile Data'},
+            {title: 'Assess Data'},
+            {title: 'Generate Report'}
+          ]
+        },
+        { title: 'Talk to the boss about making a really long description for the action-item',
+          checklist: [
+            {title: 'Talk'}
+          ]
+        }
       ]
-
-
-
-    }
+    };
   }
 });
