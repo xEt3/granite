74471ae07c96b3fea63139b6db4ebd52dab0da13
J Scott Chapman <scott8675309@gmail.com>
2016-10-19 10:13:09 -0600

    Added model hook for index route

--- app/routes/account/action-item/index.js
+++ app/routes/account/action-item/index.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const { Route, RSVP } = Ember;
 
 export default Route.extend({
-  
+  model() {
+    let actionItem = this.modelFor('account.action-item');
+    return RSVP.hash({
+      actionItem,
+      //dependents are other action-items that are
+      //waiting on this action item
+      dependents: this.store.query('action-item',
+      { prerequisites: {$in: [actionItem.get('id')] }
+      })
+    });
+  },
+
+  setupController ( controller, model ) {
+    controller.setProperties({
+      model: model.actionItem,
+      dependents: model.dependents
+    });
+  }
 });
