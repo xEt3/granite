0398dfa17a1d4ae05e3c19f16398494d8c1179be
James-Mac <james@aehr.org>
2016-10-19 07:35:51 -0600

    More action-item bootstrapping

--- app/models/checklist-item.js
+++ app/models/checklist-item.js
@@ -3,10 +3,11 @@ import Model from 'ember-data/model';
 const { attr, belongsTo } = DS;
 
 export default Model.extend({
+  name:        attr('string'),
   completedOn: attr('date'),
-  assignedOn: attr('date'),
-  priority: attr('number'),
-  assignedTo: belongsTo('company-user', { async: true }),
-  assignedBy: belongsTo('company-user', { async: true }),
+  assignedOn:  attr('date'),
+  priority:    attr('number'),
+  assignedTo:  belongsTo('company-user', { async: true }),
+  assignedBy:  belongsTo('company-user', { async: true }),
   completedBy: belongsTo('company-user', { async: true })
 });
