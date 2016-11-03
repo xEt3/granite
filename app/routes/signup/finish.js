c5d28a8295c3ddc832e1c48fde91f22ee4f8ee01
James-Mac <james@aehr.org>
2016-11-03 09:13:49 -0600

    Changes to company setup

--- app/routes/signup/finish.js
+++ app/routes/signup/finish.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, run } = Ember;
+
+export default Route.extend({
+  afterModel (model) {
+    run.scheduleOnce('afterRender', () => {
+      run.later(() => this.transitionTo('account.index'), 4500);
+    });
+  }
 });
