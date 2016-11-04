20a9a79a1ac31687aa6953df9874157f6f7ec144
James-Mac <james@aehr.org>
2016-11-04 10:20:20 -0600

    Installed `ember-cli-fix-all-your-tests-without-any-work-o-matic-5000`

--- tests/integration/components/display/version-info-test.js
+++ tests/integration/components/display/version-info-test.js
@@ -11,14 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{display/version-info}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#display/version-info}}
-      template block text
-    {{/display/version-info}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim().toLowerCase(), 'show version info');
 });
