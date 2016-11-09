1912cd251b569f9fe0c361a5e415020d15625b79
James-Mac <james@aehr.org>
2016-11-09 07:30:31 -0700

    Well actually

--- app/components/input/social-security.js
+++ app/components/input/social-security.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
+const { run, computed } = Ember;
 
 export default Ember.Component.extend({
   tagName: '',
+  showSsn: true,
+
+  didInsertElement () {
+    this._super(...arguments);
+    run.later(() => this.set('showSsn', false), 800);
+  },
+
   ssnInputType: computed('showSsn', function () {
     return this.get('showSsn') ? 'text' : 'password';
   })
