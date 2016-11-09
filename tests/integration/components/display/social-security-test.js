7689e5fa7780265f518cd61d051dc3c88d9c2292
J Scott Chapman <scott8675309@gmail.com>
2016-11-09 07:24:53 -0700

    Initial commit

--- tests/integration/components/display/social-security-test.js
+++ tests/integration/components/display/social-security-test.js
@@ -2,14 +2,14 @@ import { moduleForComponent, test } from 'ember-qunit';
 import wait from 'ember-test-helpers/wait';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('display/social-security', 'Integration | Component | display/social security', {
+moduleForComponent('input/social-security', 'Integration | Component | display/social security', {
   integration: true
 });
 
 test('it renders', function(assert) {
   assert.expect(4);
 
-  this.render(hbs`{{display/social-security}}`);
+  this.render(hbs`{{input/social-security}}`);
 
   assert.ok(this.$('input')[0], 'should contain input');
   assert.equal(this.$('input').attr('type'), 'password', 'should default to password');
