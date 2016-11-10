b3d09628af62519c4fafbd8dfa1f62ebe8534ad4
James-Mac <james@aehr.org>
2016-11-10 12:38:52 -0700

    Add favicon and app manifest, finish up initial document item display

--- app/components/list-item/document-item.js
+++ app/components/list-item/document-item.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
-const { Component, computed } = Ember;
+const { Component, K, computed } = Ember;
 
 const DocumentItemComponent = Component.extend({
-  tagName: '',
-  imagePreview: computed.match('file.extension', /je?pg|png|gif/i)
+  classNames: [ 'item' ],
+  classNameBindings: [ 'file.isDeleted:negative' ],
+  imagePreview: computed.match('file.extension', /je?pg|png|gif/i),
+  onDelete: K,
+
+  actions: {
+    delete () {
+      this.get('file').destroyRecord().then(this.get('onDelete'));
+    }
+  }
 });
 
 DocumentItemComponent.reopenClass({
