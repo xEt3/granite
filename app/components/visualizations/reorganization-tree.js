8f28c378f52ca16a4d073ede1696c059b94045d5
James-Mac <james@aehr.org>
2016-11-15 15:11:35 -0700

    D3 is literally stabbing me lifeless

--- app/components/visualizations/reorganization-tree.js
+++ app/components/visualizations/reorganization-tree.js
@@ -1,23 +1,28 @@
 import Ember from 'ember';
+import d3 from 'd3-selection';
+import { tree, hierarchy } from 'd3-hierarchy';
 
-const { Component, RSVP, inject, computed, get, set } = Ember,
+const { Component, RSVP, inject, computed, get, set, on, run } = Ember,
       select = 'name email supervisor';
 
 export default Component.extend({
+  tagName: 'svg',
   ajax: inject.service(),
+  classNames: [ 'visualization__org-tree' ],
+  attributeBindings: [ 'width', 'height' ],
 
   populateChildNodes (node) {
     let ajax = this.get('ajax');
     set(node, 'children', []);
 
-    return ajax.get('/api/v1/employees', {
+    return ajax.request('/api/v1/employees', {
       data: {
         select,
         supervisor: get(node, '_id')
       }
     })
     .then(res => {
-      let children = res.body.employees;
+      let children = res.employee;
 
       if ( children && children.length > 0 ) {
         return RSVP.map(children, child =>
@@ -35,20 +40,82 @@ export default Component.extend({
     let ajax = this.get('ajax'),
         _id = get(node, 'supervisor');
 
-    return _id ? ajax.get('/api/v1/employees', {
+    return _id ? ajax.request('/api/v1/employees', {
       data: { select, _id }
     })
-    .then(res => res.body.employees[0]) : null;
+    .then(res => res.employee[0]) : RSVP.Promise.resolve(null);
   },
 
   _simulation: computed('baseNode', function () {
     let baseNode = this.get('baseNode'),
-        tree;
+        t;
 
     return this.getParentNode(baseNode)
     .then(parentNode => {
-      tree = Ember.Object.create(parentNode || baseNode);
-      return this.populateChildNodes(tree);
+      t = Ember.Object.create(parentNode || baseNode);
+      return this.populateChildNodes(t);
+    });
+  }),
+
+  _baseRender: on('didInsertElement', function () {
+    this.setProperties({
+      width: this.$().width(),
+      height: this.$().height()
+    });
+
+    run.next(() => {
+      var svg = d3.select('#' + this.get('elementId')),
+          width = svg.attr('width'),
+          height = svg.attr('height'),
+          g = svg.append('g').attr('transform', 'translate(80,0)'),
+          t = tree().size([height, width - 160]);
+      console.log(g);
+      this.setProperties({
+        svg,
+        tree: t,
+        g,
+        width,
+        height
+      });
+    });
+  }),
+
+  _dataUpdate: computed('_simulation', function () {
+    return this.get('_simulation')
+    .then(data => {
+      run.scheduleOnce('afterRender', () => {
+        let root = hierarchy(data),
+            g = this.get('g');
+        console.log('here', g);
+        g.selectAll('.link')
+          .data(this.get('tree')(root).descendants().slice(1))
+          .enter().append('path')
+            .attr('class', 'link')
+            .attr('d', d => {
+              return 'M' + d.y + ',' + d.x +
+                     'C' + (d.y + d.parent.y) / 2 + ',' + d.x +
+                     ' ' + (d.y + d.parent.y) / 2 + ',' + d.parent.x +
+                     ' ' + d.parent.y + ',' + d.parent.x;
+            });
+
+        let node = g.selectAll('.node')
+          .data(root.descendants())
+          .enter().append('g')
+            .attr('class', d => 'node' + (d.children ? ' node--internal' : ' node--leaf'))
+            .attr('transform', d => 'translate(' + d.y + ',' + d.x + ')');
+
+        node.append('circle')
+            .attr('r', 2.5);
+
+        node.append('text')
+            .attr('dy', 3)
+            .attr('x', d => d.children ? -8 : 8)
+            .style('text-anchor', d => d.children ? 'end' : 'start')
+            .text(d => {
+              console.log(d);
+              return d.data.name.first + ' ' + d.data.name.last
+            });
+      });
     });
   })
 });
