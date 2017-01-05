08d2768075ece007192b64f196eca601086fd9bb
J Scott Chapman <scott8675309@gmail.com>
2017-01-05 15:28:18 -0700

    Added form element and screening

---
+++ tests/unit/controllers/account/job-opening/setup/screening-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:account/job-opening/setup/screening', 'Unit | Controller | account/job opening/setup/screening', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
