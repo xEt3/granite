6c44ee2b15e528ac9e9b9d6af2dd673b84c9f720
James-Mac <james@aehr.org>
2017-01-11 14:54:11 -0700

    :lipstick:

--- app/controllers/account/job-opening/setup/finish.js
+++ app/controllers/account/job-opening/setup/finish.js
@@ -4,8 +4,6 @@ const { Controller, computed } = Ember;
 
 export default Controller.extend({
   sourcesList: computed('model.applicantSources.@each.name', function () {
-    return this.get('model.applicantSources').map(source =>
-      source.get('name')
-    ).join('<br>');
+    return this.get('model.applicantSources').mapBy('name').join('<br>');
   })
 });
