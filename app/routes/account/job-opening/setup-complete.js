ca5eac2f94e8163c0496a1ca7cf989bb6dd7753e
James-Mac <james@aehr.org>
2017-01-13 09:41:03 -0700

    :stars:

--- app/routes/account/job-opening/setup-complete.js
+++ app/routes/account/job-opening/setup-complete.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, run } = Ember;
+
+export default Route.extend({
+  afterModel (model) {
+    model.setProperties({
+      setup: false,
+      setupStep: null,
+      setupProgress: null,
+      completedSetup: new Date()
+    });
+
+    model.save().then(() => run.scheduleOnce('afterRender', () => run.later(() => {
+      this.transitionTo('account.job-opening');
+    }, 5000)));
+  }
 });
