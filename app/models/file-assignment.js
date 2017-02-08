1e5d4444de4570ac81b06ed456af0213eb78196d
J Scott Chapman <scott8675309@gmail.com>
2017-02-08 11:17:28 -0700

    Added file-assignment model.

---
+++ app/models/file-assignment.js
@@ -0,0 +1,20 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default Model.extend({
+  message:           attr('string'),
+  signatureRequired: attr('boolean'),
+  signature:         attr('string'),
+  readOn:            attr('date'),
+  creator:           belongsTo('employee'),
+  company:           belongsTo('company'),
+  employee:          belongsTo('employee'),
+  file:              belongsTo('file'),
+
+  created: attr('date', {
+    defaultValue () {
+      return new Date();
+    }
+  })
+});
