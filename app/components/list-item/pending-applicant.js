dcd6149c14c97b187feac1c76bd5183c4da6d52a
James-Mac <james@aehr.org>
2017-04-21 16:47:04 -0600

    Make items selectable

--- app/components/list-item/pending-applicant.js
+++ app/components/list-item/pending-applicant.js
@@ -1,4 +1,23 @@
+import Ember from 'ember';
 import BaseLiComponent from './base';
 
+const { A, computed } = Ember;
+
 export default BaseLiComponent.extend({
+  classNameBindings: [ 'isSelected:is-selected' ],
+
+  isSelected: computed('selected.[]', 'model', function () {
+    return (this.get('selected') || A()).contains(this.get('model'));
+  }),
+
+  click (e) {
+    e.preventDefault();
+    this.send('select');
+  },
+
+  actions: {
+    select () {
+      this.get('onSelectChange')(this.get('model'), this.get('isSelected'));
+    }
+  }
 });
--- app/controllers/account/job-opening/campaign/applicant-tracking.js
+++ app/controllers/account/job-opening/campaign/applicant-tracking.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 
-const { Controller } = Ember;
+const { Controller, A } = Ember;
 
 export default Controller.extend({
+  selectedApplications: A(),
+
   actions: {
     toggleProperty (prop) {
       this.toggleProperty(prop);
+    },
+
+    selectApplication (app, remove) {
+      this.get('selectedApplications')[remove ? 'removeObject' : 'addObject'](app);
+    },
+
+    selectAllApplications () {
+      this.set('selectedApplications', A([ ...this.get('model.pendingApplications').toArray() ]));
+    },
+
+    deselectAllApplications () {
+      this.set('selectedApplications', A());
     }
   }
 });
