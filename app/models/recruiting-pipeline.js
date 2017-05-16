69158626e0b86d85665675a245054e7bb10c002a
James-Mac <james@aehr.org>
2017-05-16 16:08:10 -0600

    Boot attributes

--- app/models/recruiting-pipeline.js
+++ app/models/recruiting-pipeline.js
@@ -1,5 +1,14 @@
-import DS from 'ember-data';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
-export default DS.Model.extend({
+export default Model.extend({
+  stages: attr('array'),
 
+  jobOpenings: hasMany('job-opening', { inverse: null }),
+  company: belongsTo('company', { inverse: null }),
+
+  created: attr('date', {
+    defaultValue: () => new Date()
+  })
 });
