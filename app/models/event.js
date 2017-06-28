0198e7b7a667f6d4c7ac636cae2b86f7406d9a63
James-Mac <james@aehr.org>
2017-06-28 10:53:46 -0600

    Event model

---
+++ app/models/event.js
@@ -0,0 +1,24 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+import resolveForTypeKey from '../utils/resolve-for-type-key';
+
+export default Model.extend({
+  scheduledFor: attr('date'),
+  title:        attr('string'),
+  description:  attr('string'),
+  notes:        attr('string'),
+
+  company:       belongsTo('company', { inverse: null }),
+  creator:       belongsTo('employee', { inverse: null }),
+  attendantType: attr('string'),
+  attendantId:   attr('string'),
+
+  created: {
+    type: Date,
+    default: Date.now,
+    index: true
+  },
+
+  attendant: resolveForTypeKey('attendant')
+});
