1a4e27b81fc23ec510a9db2605b52a6d640edd05
James-Mac <james@aehr.org>
2017-07-05 15:45:48 -0600

    Adds date-range prototype

---
+++ app/components/input/date-range.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+const { Component, computed } = Ember;
+
+export default Component.extend({
+  endOptions: computed('start', function () {
+    const start = this.get('start'),
+          loopDate = moment(start),
+          opts = [];
+
+    for (let i = 0; i < 12; i++) {
+      loopDate.add(15, 'minutes');
+
+      let diffHours = loopDate.diff(start, 'hours', true),
+          diffMins = diffHours % 1 * 60,
+          diffs = [];
+
+      diffHours = Math.floor(diffHours);
+
+      if (diffHours) {
+        diffs.push(`${diffHours} ${diffHours > 1 ? 'hours' : 'hour'}`);
+      }
+
+      if (diffMins) {
+        diffs.push(`${diffMins} minutes`);
+      }
+
+      opts.push({
+        value: loopDate.toDate(),
+        label: `${moment(loopDate).format('h:mma')} (${diffs.join(' ')})`
+      });
+    }
+
+    return opts;
+  })
+});
