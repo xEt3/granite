ae1e49f2d1dfc0c17aff47bf64d602903c2a8151
James-Mac <james@aehr.org>
2017-10-02 16:01:14 -0600

    Finish up template editing poc

--- app/routes/account/settings/templates/add.js
+++ app/routes/account/settings/templates/add.js
@@ -1,14 +1,39 @@
 import Ember from 'ember';
 import add from 'granite/mixins/route-abstractions/add';
 
-const { Route } = Ember;
+const {
+  Route,
+  RSVP: { hash },
+  inject: { service }
+} = Ember;
 
 export default Route.extend(add, {
+  ajax: service(),
   modelName: 'template',
 
   getModelDefaults (params) {
-    return {
-      key: params.template_key
-    };
+    return this.get('ajax').request(`/api/v1/template/${params.template_key}/default`)
+    .then(({ template }) => {
+      const content = template.content;
+
+      return {
+        key: params.template_key,
+        content
+      };
+    });
+  },
+
+  model (params) {
+    return hash({
+      template: this._super(...arguments),
+      definition: this.store.query('template-definition', { key: params.template_key })
+    });
+  },
+
+  setupController (controller, model) {
+    controller.setProperties({
+      model: model.template,
+      definition: model.definition.get('firstObject')
+    });
   }
 });
--- app/routes/account/settings/templates/edit.js
+++ app/routes/account/settings/templates/edit.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
+import edit from 'granite/mixins/route-abstractions/edit';
 
-export default Ember.Route.extend({
+const { Route } = Ember;
+
+export default Route.extend(edit, {
+  modelName: 'template',
+  segmentKey: 'template_id',
+
+  model () {
+    return this._super(...arguments)
+    .then(template => {
+      return this.store.query('template-definition', { key: template.get('key') })
+      .then(definition => ({
+        template,
+        definition: definition.get('firstObject')
+      }));
+    });
+  },
+
+  setupController (controller, model) {
+    controller.setProperties({
+      model: model.template,
+      definition: model.definition
+    });
+  }
 });
