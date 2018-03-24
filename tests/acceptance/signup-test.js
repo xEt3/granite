942661405fa8291e6502891a03afd1feebb7ab6e
JavaLavaMT <chererjeremy@gmail.com>
2018-03-24 08:19:10 -0600

    Added some new test to sign up
    added new test to test the model. i also correted the format of signup test

--- tests/acceptance/signup-test.js
+++ tests/acceptance/signup-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, find, fillIn, triggerKeyEvent, click, triggerEvent, pauseTest } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { visit, currentURL, click, find, fillIn, settled } from '@ember/test-helpers';
 import Mirage, { faker } from 'ember-cli-mirage';
+
 
 module('Acceptance | signup', function(hooks) {
   setupApplicationTest(hooks);
@@ -14,66 +15,63 @@ module('Acceptance | signup', function(hooks) {
 
   test('filling in signup form', async function(assert) {
     await visit('/signup');
-      var el = document.querySelector("[name='phone-number']");
-  const company=faker.company.companyName();
-  const firstName=faker.name.firstName();
-  const middleName=faker.name.firstName();
-  const lastName=faker.name.lastName();
-  const email=faker.internet.email();
-  const phone=faker.phone.phoneNumber();
-  const extension=faker.random.number();
-  const street=faker.address.streetAddress();
-  const suite=faker.random.number();
-  const city=faker.address.city();
-  const state=faker.address.state();
-  const zip=faker.address.zipCode();
-  await fillIn('#company-name input', company);
-  await fillIn('#first-name input', firstName);
-  await fillIn('input[placeholder="Middle(Optional)"]', middleName);
-  await fillIn('#last-name input', lastName);
-  await fillIn('#company-email input', email);
-  await fillIn('input[placeholder="Phone Number"]', phone);
-  await fillIn('input[placeholder="Extension"]', extension);
-  await fillIn('#street-address input', street);
-  await fillIn('#suite-number input', suite);
-  await fillIn('#city input', city);
-  //await fillIn('dropdown[class="search selection"]', state);
-  await fillIn('#zipcode input', zip);
+    const controller = this.owner.lookup('controller:signup/index');
+    const fakeData = {
+      company:faker.company.companyName(),
+      firstName:faker.name.firstName(),
+      middleName:faker.name.firstName(),
+      lastName:faker.name.lastName(),
+      email:faker.internet.email(),
+      phone:faker.phone.phoneNumber(),
+      extension:faker.random.number(),
+      street:faker.address.streetAddress(),
+      suite:faker.random.number(),
+      city:faker.address.city(),
+      state:faker.address.state(),
+      zip:faker.address.zipCode()
+    };
 
-//assert.ok(find('input[data-id="{{state}}"]'));
-//assert.ok(find("div[class='default text']"),"Monatana");
+    assert.ok(!controller.get('model.name'),'Company name on model is undefined before fillIn');
+    await fillIn('#company-name input', fakeData.company);
+    await fillIn('#first-name input', fakeData.firstName);
+    await fillIn('input[placeholder="Middle(Optional)"]', fakeData.middleName);
+    await fillIn('#last-name input', fakeData.lastName);
+    await fillIn('#company-email input', fakeData.email);
+    await fillIn('input[placeholder="Phone Number"]', fakeData.phone);
+    await fillIn('input[placeholder="Extension"]', fakeData.extension);
+    await fillIn('#street-address input', fakeData.street);
+    await fillIn('#suite-number input', fakeData.suite);
+    await fillIn('#city input', fakeData.city);
+    await fillIn('#zipcode input', fakeData.zip);
+    await fillIn('#url-prefix input', fakeData.company);
 
-
-
-
-  assert.equal(find('#company-name input').value,company,"the company is "+ company);
-  assert.equal(find('#first-name input').value,firstName,"First name is "+ firstName);
-  assert.equal(find('input[placeholder="Middle(Optional)"]').value,middleName,"Middle name is "+ middleName);
-  assert.equal(find('#last-name input').value,lastName,"Last name is "+ lastName);
+    assert.equal(controller.get('model.name'),fakeData.company, `Company name on model is "${fakeData.company}" afer fillIn`);
+    assert.equal(find('#first-name input').value,fakeData.firstName,'First name is ' + fakeData.firstName);
+    assert.equal(find('input[placeholder="Middle(Optional)"]').value,fakeData.middleName,'Middle name is ' + fakeData.middleName);
+    assert.equal(find('#last-name input').value,fakeData.lastName,'Last name is ' + fakeData.lastName);
 
 
-  assert.equal(find("input[placeholder='Phone Number']").value,phone,'the phone number is '+phone);
-  assert.equal(find("input[placeholder='Extension']").value,extension,'the phone number extention is '+ extension);
-  assert.equal(find('#company-email input').value,email,"the Email is "+ email);
-  assert.equal(find('#street-address input').value,street,"the Address is "+ street);
-  assert.equal(find('#suite-number input').value,suite,"the Suite is "+ suite);
-  assert.equal(find('#city input').value,city,"the city is "+ city);
-  assert.equal(find('#zipcode input').value,zip,"the zip is "+ zip);
+    assert.equal(find('input[placeholder=\'Phone Number\']').value,fakeData.phone,'the phone number is ' + fakeData.phone);
+    assert.equal(find('input[placeholder=\'Extension\']').value,fakeData.extension,'the phone number extention is ' + fakeData.extension);
+    assert.equal(find('#company-email input').value,fakeData.email,'the Email is ' + fakeData.email);
+    assert.equal(find('#street-address input').value,fakeData.street,'the Address is ' + fakeData.street);
+    assert.equal(find('#suite-number input').value,fakeData.suite,'the Suite is ' + fakeData.suite);
+    assert.equal(find('#city input').value,fakeData.city,'the city is ' + fakeData.city);
+    assert.equal(find('#zipcode input').value,fakeData.zip,'the zip is ' + fakeData.zip);
+    assert.equal(find('#url-prefix input').value,fakeData.company, 'urlPrefix shows ' + fakeData.company);
 
+    await click('.select__address-state');
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    assert.ok(find('.select__address-state .menu.visible'), 'Menu gets the visible class');
+    await click('.select__address-state .select-address-state__item[data-id="Montana"]');
+    await settled();
+    assert.equal(controller.get('model.addressState'), 'Montana');
 
 
 
   });
-  test('state dropdown works with click', async function(assert) {
-    await visit('/signup');
 
-
-    //await click('#test');
-
-triggerEvent('#test','click');
-await pauseTest();
-          assert.equal(find("#test"),find("div[data-id='Alabama']"), "states are shown ");
-      });
 
 
 
