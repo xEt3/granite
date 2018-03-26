031b93b45ea60d3854fca085d45ad59a7317f178
J Scott Chapman <scott8675309@gmail.com>
2018-03-25 18:34:48 -0600

    Completed results and got rid of scheduleOnce

--- app/controllers/account/employee/offboard/options.js
+++ app/controllers/account/employee/offboard/options.js
@@ -6,21 +6,5 @@ import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 
 export default Controller.extend(addEdit, {
   states,
-  stateIsMontana: computed.equal('model.addressState', 'MT'),
-
-  init () {
-    this._super(...arguments);
-
-    // run.scheduleOnce('afterRender', () => {
-    //   let model = this.get('model');
-    //
-    //   model.setProperties({
-    //     finalAddressLine1: model.get('addressLine1'),
-    //     finalAddressLine2: model.get('addressLine2'),
-    //     finalAddressCity: model.get('addressCity'),
-    //     finalAddressZip: model.get('addressZip'),
-    //     finalAddressState: model.get('addressState')
-    //   });
-    // });
-  }
+  stateIsMontana: computed.equal('model.addressState', 'MT')
 });
