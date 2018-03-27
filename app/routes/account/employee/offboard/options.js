774856b895e8b74293860aceb44f54bfd0305681
J Scott Chapman <scott8675309@gmail.com>
2018-03-27 06:02:53 -0600

    Removed "unless" block and added model defaults to setupController method.

--- app/routes/account/employee/offboard/options.js
+++ app/routes/account/employee/offboard/options.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+
+  setupController(model, controller) {
+    this._super(controller, model)
+    controller.setProperties({
+      finalAddressLine1: model.get('addressLine1'),
+      finalAddressLine2: model.get('addressLine2'),
+      finalAddressCity: model.get('addressCity'),
+      finalAddressZip: model.get('addressZip'),
+      finalAddressState: model.get('addressState')
+    })
+  }
+});
