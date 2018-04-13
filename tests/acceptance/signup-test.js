d1a2dd7792585809d18e6ed35ccff6606081c668
James <james@aehr.org>
2018-04-13 09:18:58 -0600

    Simplifying w/ Jeremy! :v:

--- tests/acceptance/signup-test.js
+++ tests/acceptance/signup-test.js
@@ -1,8 +1,22 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { visit, currentURL, click, find, fillIn, settled, pauseTest} from '@ember/test-helpers';
+import { visit, currentURL, click, find, fillIn, settled } from '@ember/test-helpers';
 import { faker } from 'ember-cli-mirage';
 
+const fieldMap = {
+  name:              '#company-name input',
+  contactFirstName:  '#first-name input',
+  contactMiddleName: 'input[placeholder="Middle(Optional)"]',
+  contactLastName:   '#last-name input',
+  email:             '#company-email input',
+  contactPhone:      'input[placeholder="Phone Number"]',
+  contactExtension:  'input[placeholder="Extension"]',
+  addressLine1:      '#street-address input',
+  addressLine2:      '#suite-number input',
+  addressCity:       '#city input',
+  addressZipCode:    '#zipcode input',
+  urlPrefix:         '#url-prefix input'
+};
 
 module('Acceptance | signup', function(hooks) {
   setupApplicationTest(hooks);
@@ -14,59 +28,39 @@ module('Acceptance | signup', function(hooks) {
     const controller = this.owner.lookup('controller:signup/index');
 
     const fakeData = {
-      company:faker.name.firstName(),
-      firstName:faker.name.firstName(),
-      middleName:faker.name.firstName(),
-      lastName:faker.name.lastName(),
-      email:faker.internet.email(),
-      phone:'(406) 567-6354',
-      extension:faker.random.number(),
-      street:faker.address.streetAddress(),
-      suite:faker.random.number(),
-      city:faker.address.city(),
-      state:faker.address.state(),
-      zip:faker.random.number({min:11111, max:99999})
+      name:              faker.company.companyName(),
+      contactFirstName:  faker.name.firstName(),
+      contactMiddleName: faker.name.firstName(),
+      contactLastName:   faker.name.lastName(),
+      email:             faker.internet.email(),
+      contactPhone:      '(406) 567-6354',
+      contactExtension:  faker.random.number(),
+      addressLine1:      faker.address.streetAddress(),
+      addressLine2:      faker.random.number(),
+      addressCity:       faker.address.city(),
+      state:             faker.address.state(),
+      addressZipCode:    faker.random.number({min:11111, max:99999}),
+      urlPrefix:         'abc'
     };
 
-    assert.ok(!controller.get('model.name'),'Company name on model is undefined before fillIn');
-    assert.ok(!controller.get('model.contactFirstName'),'First Name on model is undefined before fillIn');
-    assert.ok(!controller.get('model.contactMiddleName'),'Middle Name on model is undefined before fillIn');
-    assert.ok(!controller.get('model.contactLastName'),'Last Name on model is undefined before fillIn');
-    assert.ok(!controller.get('model.email'),'Email on model is undefined before fillIn');
-    assert.ok(!controller.get('model.contactPhone'),'Phone on model is undefined before fillIn');
-    assert.ok(!controller.get('model.contactExtension'),'Extention on model is undefined before fillIn');
-    assert.ok(!controller.get('model.contactExtension'),'Extention on model is undefined before fillIn');
-    assert.ok(!controller.get('model.addressLine1'),'Street address on model is undefined before fillIn');
-    assert.ok(!controller.get('model.addressLine2'),'Suite on model is undefined before fillIn');
-    assert.ok(!controller.get('model.addressCity'),'City on model is undefined before fillIn');
-    assert.ok(!controller.get('model.addressZipCode'),'Zip on model is undefined before fillIn');
+    let model = controller.get('model');
 
-    await fillIn('#company-name input', fakeData.company);
-    await fillIn('#first-name input', fakeData.firstName);
-    await fillIn('input[placeholder="Middle(Optional)"]', fakeData.middleName);
-    await fillIn('#last-name input', fakeData.lastName);
-    await fillIn('#company-email input', fakeData.email);
-    await fillIn('input[placeholder="Phone Number"]', fakeData.phone);
-    await fillIn('input[placeholder="Extension"]', fakeData.extension);
-    await fillIn('#street-address input', fakeData.street);
-    await fillIn('#suite-number input', fakeData.suite);
-    await fillIn('#city input', fakeData.city);
-    await fillIn('#zipcode input', fakeData.zip);
-    await fillIn('#url-prefix input', fakeData.company);
+    // Assure that all fields are undefined
+    Object.keys(fieldMap).forEach(key =>
+      assert.ok(!model.get(key), `model.${key} is undefined before fillIn`));
 
-    assert.equal(controller.get('model.name'),fakeData.company, `Company name on model is "${fakeData.company}" afer fillIn`);
+    // Fill in fields
+    for (let key in fieldMap) {
+      if (!fieldMap.hasOwnProperty(key)) {
+        continue;
+      }
 
-    assert.equal(controller.get('model.contactFirstName'),fakeData.firstName, `First Name on model is "${fakeData.firstName}" after fillIn`);
-    assert.equal(controller.get('model.contactMiddleName'),fakeData.middleName, `Middle Name on model is "${fakeData.middleName}" after fillIn`);
-    assert.equal(controller.get('model.contactLastName'),fakeData.lastName, `Last Name on model is "${fakeData.lastName}" after fillIn`);
-    assert.equal(controller.get('model.email'),fakeData.email, `Email on model is "${fakeData.email}" after fillIn`);
-    assert.equal(controller.get('model.contactPhone'),fakeData.phone,`Phone on model is "${fakeData.phone}" after fillIn`);
-    assert.equal(controller.get('model.contactExtension'),fakeData.extension,`Extention on model is "${fakeData.extension}" after fillIn`);
-    assert.equal(controller.get('model.addressLine1'), fakeData.street,`Street address on model is "${fakeData.street}" after fillIn`);
-    assert.equal(controller.get('model.addressLine2'),fakeData.suite,`Suite on model is "${fakeData.suite}" after fillIn`);
-    assert.equal(controller.get('model.addressCity'),fakeData.city,`City on model is "${fakeData.city}" after fillIn`);
-    assert.equal(controller.get('model.addressZipCode'),fakeData.zip,`Zip on model is "${fakeData.zip}" after fillIn`);
-    assert.equal(controller.get('model.urlPrefix'),fakeData.company,`URL on model is https://"${fakeData.company}".granitehr.com after fillIn`);
+      await fillIn(fieldMap[key], fakeData[key]);
+    }
+
+    // Assure filled in fields are set on the model
+    Object.keys(fieldMap).forEach(key =>
+      assert.equal(model.get(key), fakeData[key], `model.${key} is "${fakeData[key]}" after fillIn`));
 
     await click('.select__address-state');
     await new Promise(resolve => setTimeout(resolve, 500));
@@ -86,7 +80,7 @@ module('Acceptance | signup', function(hooks) {
     billingController.set('nonce', 'fake-valid-nonce');
     await settled();
 
-    let company = server.db.companies.where({ name: fakeData.company })[0];
+    let company = server.db.companies.where({ name: fakeData.name })[0];
     assert.ok(company, 'Company exists');
     assert.ok(!find('button[type="submit"]').disabled, 'Submit is enabled');
 
@@ -102,5 +96,4 @@ module('Acceptance | signup', function(hooks) {
     await settled();
     assert.equal(currentURL(), '/', 'brought you back to the index page');
   });
-
 });
