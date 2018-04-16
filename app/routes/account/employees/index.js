b05895d08abd9a4bda2902c08f786e8a36b5ef87
petersonbrock <brock@aehr.org>
2018-04-16 16:02:47 -0600

    initial look at adding an employee filter

--- app/routes/account/employees/index.js
+++ app/routes/account/employees/index.js
@@ -1,11 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // FILTER REFRESH MODEL TRUE HERE?
   queryParams: {
     page: { refreshModel: true }
   },
 
   model ( params ) {
+    // WILL ADD QUERY HERE
     let limit = this.get('controller.limit') || 20,
         page = (params.page || 1) - 1;
 
