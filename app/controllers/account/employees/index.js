4d647b25702819910e7f33f659a7a434a2e503b2
petersonbrock <brock@aehr.org>
2018-04-17 15:13:29 -0600

    working through employee filter functionality

--- app/controllers/account/employees/index.js
+++ app/controllers/account/employees/index.js
@@ -3,10 +3,16 @@ import { computed } from '@ember/object';
 import pagination from 'granite/mixins/controller-abstractions/pagination';
 
 export default Controller.extend(pagination, {
-  queryParams: [ 'page' ],
-  onboardingToggled: false,
-  filterCategories: ['hireDate', 'onboarding', 'supervisor', 'department', 'location'],
+  queryParams: ['expandFiltered', 'onboarding', 'supervisedBy', 'page'],
+
+  filterCategories: ['onboarding', 'hireDate', 'supervisor', 'department', 'location'],
   limit: 20,
+  expandFiltered: false,
+  onboarding: false,
+  supervisorToggle: false,
+  supervisedBy: null,
+  departmentToggle: false,
+  department: null,
 
   intros: computed(function () {
     return [{
@@ -26,5 +32,25 @@ export default Controller.extend(pagination, {
       intro: 'When you need to add an employee, click the add button and pick from a single employee or multiple (via CSV or Excel)',
       position: 'left'
     }];
-  })
+  }),
+
+  actions : {
+    changeFilter (filterToChange) {
+      let filter = this.get('filters');
+      if(filter.includes(filterToChange)){
+        filter.removeObject(filterToChange)
+      } else {
+        filter.addObject(filterToChange);
+      }
+    },
+
+    toggleProperty (toToggle) {
+      this.toggleProperty(toToggle);
+      if(toToggle==='supervisorToggle'){
+        this.set('supervisedBy', null);
+      } else if (toToggle==='departmentToggle') {
+        this.set('department', null);
+      }
+    }
+  }
 });
--- app/routes/account/employees/index.js
+++ app/routes/account/employees/index.js
@@ -1,19 +1,71 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
-  // FILTER REFRESH MODEL TRUE HERE?
   queryParams: {
-    page: { refreshModel: true }
+    expandFiltered: { refreshModel: false },
+    onboarding: { refreshModel: true },
+    supervisedBy: { refreshModel: true },
+    department: { refreshModel: true },
+    page: { refreshModel: true },
   },
 
   model ( params ) {
-    // WILL ADD QUERY HERE
     let limit = this.get('controller.limit') || 20,
         page = (params.page || 1) - 1;
 
-    return this.store.query('employee', {
+    let employeeQuery = {
+      limit,
       page,
-      limit
+      sort: { created: -1 }
+    };
+
+    if(params.onboarding){
+      employeeQuery.onboarding = true;
+    }
+
+    if(params.supervisedBy){
+      employeeQuery.supervisor = params.supervisedBy;
+    }
+
+    if(params.department){
+      employeeQuery.department = params.department;
+    }
+
+    // if(!isEmpty(params.filters)){
+    //   let filters = params.filters;
+    //   if(filters.includes('onboarding')){
+    //     employeeQuery.onboarding = true;
+    //   }
+    //   if (filters.includes('supervisor')){
+    //     console.log('supervisor if', params.selectedSupervisor);
+    //     console.log('employeeQuery', employeeQuery);
+    //
+    //     // employeeQuery.supervisor = params.selectedSupervisor.id;
+    //   }
+    //
+    // }
+
+    return hash({
+      allEmployees: this.store.query('employee', {}),
+      allDepartments: this.store.query('department', {}),
+      employees: this.store.query('employee', employeeQuery),
+      params
+    });
+  },
+
+  setupController ( controller, model ) {
+    this._super(...arguments);
+    controller.setProperties({
+      model: model.employees,
+      allEmployees: model.allEmployees,
+      allDepartments: model.allDepartments,
+      expandFiltered: model.params.expandFiltered,
+      onboarding: model.params.onboarding,
+      supervisorToggle: !!model.params.supervisedBy,
+      supervisedBy: model.params.supervisedBy,
+      departmentToggle: !!model.params.department,
+      department: model.params.department
     });
   }
 });
