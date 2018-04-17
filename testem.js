ac68f6e45cecd375540a0fa75a7d1415a3565e40
James <james@aehr.org>
2018-04-17 14:00:34 -0600

    :lipstick:

--- testem.js
+++ testem.js
@@ -8,7 +8,7 @@ module.exports = {
     "SL_chrome_public_56_0_Windows_10",
     "SL_chrome_public_beta_Windows_10",
     "SL_chrome_public_65_0_Windows_10",
-    "SL_firefox_public_59_0_Windows_10",
+    "SL_firefox_public_59_0_Windows_10"
     // "SL_safari_public_11_0_macOS_10_13"
   ],
 
@@ -32,24 +32,24 @@ module.exports = {
   },
 
   launchers: {
-    SL_MicrosoftEdge_public_16_16299_Windows_10: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "MicrosoftEdge",
-        "-v",
-        "16.16299",
-        "--visibility",
-        "public",
-        "-p",
-        "Windows 10",
-        "--attach",
-        "--no-connect",
-        "--url"
-      ],
-      "protocol": "browser"
-    },
+    // SL_MicrosoftEdge_public_16_16299_Windows_10: {
+    //   "exe": "ember",
+    //   "args": [
+    //     "sauce:launch",
+    //     "-b",
+    //     "MicrosoftEdge",
+    //     "-v",
+    //     "16.16299",
+    //     "--visibility",
+    //     "public",
+    //     "-p",
+    //     "Windows 10",
+    //     "--attach",
+    //     "--no-connect",
+    //     "--url"
+    //   ],
+    //   "protocol": "browser"
+    // },
 
     SL_chrome_public_56_0_Windows_10: {
       "exe": "ember",
