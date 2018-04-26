3e0fde41524228443c3b4be53808f31095dc15af
James <james@aehr.org>
2018-04-26 08:44:08 -0600

    Increase coverage, slim section html

--- tests/integration/components/display/filter-pane-test.js
+++ tests/integration/components/display/filter-pane-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click } from '@ember/test-helpers';
+import { render, click, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | display/filter-pane', function(hooks) {
@@ -13,19 +13,30 @@ module('Integration | Component | display/filter-pane', function(hooks) {
       updateCalled = Array.prototype.slice.call(arguments);
     });
 
+    this.set('aVar', false);
+
     await render(hbs`
-      {{#display/filter-pane onChange=(action someAction) as |filter|}}
-        {{#filter.section 'Some Section'}}
-          {{#filter.control 'aValue' true as |update|}}
+      {{#display/filter-pane onChange=(action someAction) class="pane" as |filter|}}
+        {{#filter.section 'Some Section' class="section"}}
+          {{#filter.control 'aValue' aVar class="control" as |update|}}
             <a href="#" {{action update false}}>update</a>
           {{/filter.control}}
         {{/filter.section}}
       {{/display/filter-pane}}
     `);
 
+    // Check section rendering, control invisibility
     assert.ok(this.element.textContent.trim().indexOf('Some Section') > -1, 'Section name is rendered.');
+    assert.ok(find('.section > div.hidden'), 'Control is not visible');
+    await click('.section .clickable.header');
 
+    // Trigger change, check control visibility
+    assert.notOk(find('.section > div.hidden'), 'Control is visible');
     await click('a[href="#"]');
     assert.deepEqual(updateCalled, [ 'aValue', false ], 'function called');
+
+    // Check reset operations
+    await click('.section .clickable.header');
+    assert.deepEqual(updateCalled, [ 'aValue', undefined ], 'function called');
   });
 });
