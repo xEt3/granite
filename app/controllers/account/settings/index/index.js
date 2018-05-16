46d2a1b1815b652d16d0af0de1daa75785e792bb
James <james@aehr.org>
2018-05-16 09:28:25 -0600

    Add address, company name. Remove palette

--- app/controllers/account/settings/index/index.js
+++ app/controllers/account/settings/index/index.js
@@ -4,19 +4,62 @@ import { Promise } from 'rsvp';
 import moment from 'moment';
 import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 import fileSupport from 'granite/mixins/file-handling';
+import { states } from 'granite/config/statics';
 
 const timezones = moment.tz.names();
 
 export default Controller.extend(addEdit, fileSupport, {
   timezones,
+  states,
 
   settingsForm: computed(() => [{
+    label: 'Company Name',
+    type: 'text',
+    path: 'name',
+    parentClass: 'sixteen wide column'
+  }, {
+    label: 'Email',
+    type: 'text',
+    path: 'email',
+    parentClass: 'sixteen wide column'
+  }, {
+    label: 'Address Line 1',
+    type: 'text',
+    path: 'addressLine1',
+    parentClass: 'sixteen wide column'
+  }, {
+    label: 'Address Line 2',
+    type: 'text',
+    path: 'addressLine2',
+    parentClass: 'sixteen wide column'
+  }, {
+    label: 'City',
+    type: 'text',
+    path: 'addressCity',
+    parentClass: 'six wide column'
+  }, {
+    label: 'State',
+    type: 'select',
+    inputClass: 'search',
+    path: 'addressState',
+    contentPath: 'controller.states',
+    displayKey: 'label',
+    valuePath: 'value',
+    parentClass: 'six wide column',
+    selectText: 'Select a State'
+  }, {
+    label: 'Zipcode',
+    type: 'text',
+    path: 'addressZipCode',
+    parentClass: 'four wide column'
+  }, {
     label: 'Timezone',
     type: 'select',
     inputClass: 'search',
     path: 'tz',
     contentPath: 'controller.timezones',
-    selectText: 'Select a Time Zone'
+    selectText: 'Select a Time Zone',
+    parentClass: 'sixteen wide column'
   }]),
 
   /* File settings */
