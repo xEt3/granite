105f6aaac22885173c414be0fb4e73d5bd8c84cc
petersonbrock <petersonbrock2@gmail.com>
2018-05-18 09:36:52 -0600

    commented out undo on pipeline card, and fixed employee hiring issue

--- app/controllers/account/job-opening/campaign/applicant-tracking.js
+++ app/controllers/account/job-opening/campaign/applicant-tracking.js
@@ -121,7 +121,7 @@ export default Controller.extend(addEdit, ajaxStatus, modalSupport, {
     employee.set('jobTitle', job.get('title'));
 
     return jobApplication.save()
-    .then(() => employee.get('content').save())
+    .then(() => employee.save())
     .then(employeeRecord => {
       if (wasNew) {
         this.transitionToRoute('account.employee.onboard', employeeRecord.get('id'));
