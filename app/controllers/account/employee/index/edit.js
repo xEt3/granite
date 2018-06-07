bd6b5f95f2b9f663d23b03d2b93251aae9778ac4
petersonbrock <petersonbrock2@gmail.com>
2018-06-07 15:26:30 -0600

    trying to find good way to find when a change to a relationship has occured

--- app/controllers/account/employee/index/edit.js
+++ app/controllers/account/employee/index/edit.js
@@ -1,14 +1,30 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, get } from '@ember/object';
 import { Promise } from 'rsvp';
+import Employee from 'granite/models/employee';
 import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 import $ from 'jquery';
+
+const employeeBelongsTo = [ 'location', 'department', 'supervisor' ];
 
 export default Controller.extend(addEdit, {
   transitionAfterSave: 'account.employee.index',
   transitionWithModel: true,
-  noDirtyModelAttributes: computed.not('model.hasDirtyAttributes'),
+  // noDirtyModelAttributes: computed.not('model.hasDirtyAttributes'),
+  noDirtyModelAttributes: computed('model.hasDirtyAttributes', function () {
+    console.log('dirty? :', this.get('model.hasDirtyAttributes'));
+    return !this.get('model.hasDirtyAttributes');
+  }),
   disabled: computed.or('loading', 'noDirtyModelAttributes'),
+  // test: computed(`model.{${employeeBelongsTo.join(',')}}`, function () {
+  //   console.log('in here');
+  // }),
+
+  test: computed(`model.[]`, function () {
+    console.log('something changed');
+    console.log('models department was:', this.get('currentDepartment'));
+    console.log('model now is:', this.get('model.department'));
+  }),
 
   actions: {
     selectEffectiveDate () {
--- app/routes/account/employee/index/edit.js
+++ app/routes/account/employee/index/edit.js
@@ -4,5 +4,11 @@ import edit from 'granite/mixins/route-abstractions/edit';
 export default Route.extend(edit, {
   model () {
     return this.modelFor('account.employee');
+  },
+
+  setupController (controller, model) {
+    controller.setProperties({
+      currentDepartment: model.department
+    });
   }
 });
