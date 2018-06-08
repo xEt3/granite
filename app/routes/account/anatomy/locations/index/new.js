b710c3f3a8f18b6b32e3057519e59de0e734b5d9
JavaLavaMT <chererjeremy@gmail.com>
2018-06-08 09:07:02 -0600

    Enhancement] when choosing a state in locations shows Alabama/Closed
    Closed#220

--- app/routes/account/anatomy/locations/index/new.js
+++ app/routes/account/anatomy/locations/index/new.js
@@ -1,15 +1,20 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import add from 'granite/mixins/route-abstractions/add';
+import {resolve} from 'rsvp';
 
 export default Route.extend(add, {
   modelName: 'location',
   auth: service(),
 
   getModelDefaults () {
-    return {
-      company: this.get('auth.user.company'),
-      creator: this.get('auth.user')
-    };
+    return resolve(this.get('auth.user.company'))
+      .then(company => {
+        return {
+          company,
+          addressState: company.addressState,
+          creator: this.get('auth.user')
+        };
+      });
   }
 });
