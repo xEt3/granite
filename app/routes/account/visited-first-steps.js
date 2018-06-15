0eee62558d41786233a72c7d3aaf30826343fb53
JavaLavaMT <chererjeremy@gmail.com>
2018-06-15 11:55:15 -0600

    update

--- app/routes/account/visited-first-steps.js
+++ app/routes/account/visited-first-steps.js
@@ -7,10 +7,24 @@ export default Route.extend({
 
   model () {
     return hash({
-      changeQueue: this.get('ajax').request('/api/v1/changes', {
+      employeeCount: this.get('ajax').request('/api/v1/employees', {
         data: {
           _count: true,
           terminatedOn: { $not: { $type: 9 } }
+        }
+      }).then(response => response && response.count),
+
+      locationCount: this.get('ajax').request('/api/v1/locations', {
+        data: {
+          _count: true,
+          name: { $not: { $type: 10 } }
+        }
+      }).then(response => response && response.count),
+
+      departmentCount: this.get('ajax').request('/api/v1/departments', {
+        data: {
+          _count: true,
+          name: { $not: { $type: 10 } }
         }
       }).then(response => response && response.count)
     });
