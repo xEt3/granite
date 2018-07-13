fbb91ee29829ce2e7f4ee63aab72f2a3955f3ff1
JavaLavaMT <chererjeremy@gmail.com>
2018-07-13 11:57:23 -0600

    changed the time
    made the time a tad longer

--- app/routes/account/first-steps.js
+++ app/routes/account/first-steps.js
@@ -63,7 +63,7 @@ export default Route.extend({
       }
 
       scheduleOnce('afterRender', () => {
-        later(() => this.transitionTo('account.index'), 3000);
+        later(() => this.transitionTo('account.index'), 4300);
       });
     });
   }
