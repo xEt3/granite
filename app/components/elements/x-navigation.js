069fd32d3f11a23e9b3df6733b7c20f227b45e19
JavaLavaMT <chererjeremy@gmail.com>
2018-07-24 09:35:46 -0600

    [wip] nav bar
    just added a drop down for the nav (still pretty rough)

--- app/components/elements/x-navigation.js
+++ app/components/elements/x-navigation.js
@@ -3,7 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   auth: service(),
+  open: false,
   tagName: 'nav',
-  classNames: [ 'ui menu nav__main' ],
-  classNameBindings: [ 'transparent:nav__main-transparent' ]
+  classNames: [ 'ui menu nav__main', 'menu__container-responsive' ],
+  classNameBindings: [ 'transparent:nav__main-transparent' ],
+
+  actions: {
+    toggleMenu () {
+      this.toggleProperty('open');
+    }
+  }
 });
