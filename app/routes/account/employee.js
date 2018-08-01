161cc044275599dc1675e45fa1c1839a2ea3a74f
JavaLavaMT <chererjeremy@gmail.com>
2018-08-01 14:18:20 -0600

    ajax for import

--- app/routes/account/employee.js
+++ app/routes/account/employee.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import refreshable from 'granite/mixins/refreshable';
 
-export default Route.extend({
+export default Route.extend(refreshable, {
   model ( params ) {
     return this.store.find('employee', params.id);
   }
