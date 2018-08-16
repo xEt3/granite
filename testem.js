29daf79db34f1ac03e5f0301f34a30df3e97b5a2
James <james@twox.io>
2018-08-16 10:01:41 -0600

    Some more CI stuff (#294)
    * Some configurator
    
    * Remove "z" ?
    
    * Increase browser start timeout
    
    * Set port to 7000
    
    * Bump safari version

--- testem.js
+++ testem.js
@@ -1,16 +1,20 @@
+'use strict';
+
 /* eslint-env node */
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  browser_disconnect_timeout: 60,
+  browser_disconnect_timeout: 120,
+  browser_start_timeout: 60,
+  port: 7000,
 
   launch_in_ci: [
-    // 'SL_MicrosoftEdge_public_16_16299_Windows_10',
-    "SL_chrome_public_56_0_Windows_10",
-    "SL_chrome_public_beta_Windows_10",
-    "SL_chrome_public_65_0_Windows_10",
-    "SL_firefox_public_59_0_Windows_10"
-    // "SL_safari_public_11_0_macOS_10_13"
+    'SL_MicrosoftEdge_public_16_16299_Windows_10',
+    'SL_chrome_public_56_0_Windows_10',
+    'SL_chrome_public_beta_Windows_10',
+    'SL_chrome_public_65_0_Windows_10',
+    'SL_firefox_public_59_0_Windows_10',
+    'SL_safari_public_11_0_macOS_10_13'
   ],
 
   launch_in_dev: [
@@ -35,117 +39,117 @@ module.exports = {
 
   launchers: {
     SL_MicrosoftEdge_public_16_16299_Windows_10: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "MicrosoftEdge",
-        "-v",
-        "16.16299",
-        "--visibility",
-        "public",
-        "-p",
-        "Windows 10",
-        "--attach",
-        "--no-connect",
-        "--url"
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'MicrosoftEdge',
+        '-v',
+        '16.16299',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 10',
+        '--attach',
+        '--no-connect',
+        '--url'
       ],
-      "protocol": "browser"
+      'protocol': 'browser'
     },
 
     SL_chrome_public_56_0_Windows_10: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "chrome",
-        "-v",
-        "56.0",
-        "--visibility",
-        "public",
-        "-p",
-        "Windows 10",
-        "--attach",
-        "--no-connect",
-        "--url"
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'chrome',
+        '-v',
+        '56.0',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 10',
+        '--attach',
+        '--no-connect',
+        '--url'
       ],
-      "protocol": "browser"
+      'protocol': 'browser'
     },
 
     SL_chrome_public_beta_Windows_10: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "chrome",
-        "-v",
-        "beta",
-        "--visibility",
-        "public",
-        "-p",
-        "Windows 10",
-        "--attach",
-        "--no-connect",
-        "--url"
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'chrome',
+        '-v',
+        'beta',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 10',
+        '--attach',
+        '--no-connect',
+        '--url'
       ],
-      "protocol": "browser"
+      'protocol': 'browser'
     },
 
     SL_chrome_public_65_0_Windows_10: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "chrome",
-        "-v",
-        "65.0",
-        "--visibility",
-        "public",
-        "-p",
-        "Windows 10",
-        "--attach",
-        "--no-connect",
-        "--url"
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'chrome',
+        '-v',
+        '65.0',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 10',
+        '--attach',
+        '--no-connect',
+        '--url'
       ],
-      "protocol": "browser"
+      'protocol': 'browser'
     },
 
     SL_firefox_public_59_0_Windows_10: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "firefox",
-        "-v",
-        "59.0",
-        "--visibility",
-        "public",
-        "-p",
-        "Windows 10",
-        "--attach",
-        "--no-connect",
-        "--url"
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'firefox',
+        '-v',
+        '59.0',
+        '--visibility',
+        'public',
+        '-p',
+        'Windows 10',
+        '--attach',
+        '--no-connect',
+        '--url'
       ],
-      "protocol": "browser"
+      'protocol': 'browser'
     },
 
     SL_safari_public_11_0_macOS_10_13: {
-      "exe": "ember",
-      "args": [
-        "sauce:launch",
-        "-b",
-        "safari",
-        "-v",
-        "11.0",
-        "--visibility",
-        "public",
-        "-p",
-        "macOS 10.13",
-        "--attach",
-        "--no-connect",
-        "--url"
+      'exe': 'ember',
+      'args': [
+        'sauce:launch',
+        '-b',
+        'safari',
+        '-v',
+        '11.1',
+        '--visibility',
+        'public',
+        '-p',
+        'macOS 10.13',
+        '--attach',
+        '--no-connect',
+        '--url'
       ],
-      "protocol": "browser"
+      'protocol': 'browser'
     }
   }
 };
