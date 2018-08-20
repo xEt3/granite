0d37515e0d8b3d1a12e6fbb684a1224ad2981535
Jeremy Cherer <chererjeremy@gmail.com>
2018-08-20 08:37:23 -0600

    [Enhancement] #279 ability to resend activation email to employees (#287)
    * added a bar
    
    * added resend email
    
    * :lipstick:
    
    * :lipstick:

--- app/controllers/account/employee/index.js
+++ app/controllers/account/employee/index.js
@@ -2,10 +2,24 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { inject as controller } from '@ember/controller';
 import del from 'granite/mixins/controller-abstractions/delete';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend(del, {
+  ajax: service(),
   application: controller(),
   transitionAfterSave: 'account.employees',
 
-  onSummary: computed.equal('application.currentPath', 'account.employee.index.index')
+  onSummary: computed.equal('application.currentPath', 'account.employee.index.index'),
+
+  actions: {
+    resend() {
+      this.ajaxStart();
+
+      return this.get('ajax').request(`/api/v1/employee/${this.model.id}/resend-activation`)
+      .then(() => {
+        this.ajaxSuccess('Email sent.');
+      })
+      .catch(this.ajaxError.bind(this));
+    }
+  }
 });
