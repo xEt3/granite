8f89a9848236011daf59b3904bb9a81af3211305
petersonbrock <petersonbrock2@gmail.com>
2018-09-25 15:59:04 -0600

    cleaned up asset-item page.  added pagination to asset-item page as well

--- app/controllers/account/asset/index.js
+++ app/controllers/account/asset/index.js
@@ -2,10 +2,15 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { inject as controller } from '@ember/controller';
 import del from 'granite/mixins/controller-abstractions/delete';
+import pagination from 'granite/mixins/controller-abstractions/pagination';
 
-export default Controller.extend(del, {
-  application: controller(),
-  addingAsset: computed.equal('application.currentPath', 'account.asset.index.new'),
+export default Controller.extend(del, pagination, {
+  queryParams:  [ 'page' ],
+  application:  controller(),
+  addingAsset:  computed.equal('application.currentPath', 'account.asset.index.new'),
+  popupContent: computed('expandAttributes', function () {
+    return this.get('expandAttributes') ? 'Hide all stock attributes' : 'Show all stock attributes';
+  }),
 
   actions: {
     toggleProperty (prop) {
