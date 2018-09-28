bf7b16280ba5933705a74d6c817f1c9bfb5f3209
Jeremy Cherer <chererjeremy@gmail.com>
2018-09-28 13:56:02 -0600

    :lipstick: added the new lines back

--- mirage/config.js
+++ mirage/config.js
@@ -26,19 +26,23 @@ const processEmbeddedRelationships = ({ model, key, data, parentId, parentKey, p
 export default function () {
   this.logging = true;
   this.namespace = '/api/v1';
+
   // These comments are here to help you get started. Feel free to delete them.
   /*
     Config (with defaults).
     Note: these only affect routes defined *after* them!
   */
+
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
   /* BT passthroughs */
   this.passthrough('https://api.sandbox.braintreegateway.com:443/**');
   this.passthrough('https://client-analytics.sandbox.braintreegateway.com/**');
 
   // Simulate login actions
+
   this.post('/login/company-user', (db, request) => {
     const params = parseIncoming(request);
 
