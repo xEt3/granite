cfb4217ac98052fa885d71a9b514e8e353752bba
petersonbrock <petersonbrock2@gmail.com>
2018-10-11 15:56:02 -0600

    unified card design for anatomy pages

--- app/controllers/account/anatomy/company-users/index.js
+++ app/controllers/account/anatomy/company-users/index.js
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
+import { inject as controller } from '@ember/controller';
+import { computed } from '@ember/object';
 import pagination from 'granite/mixins/controller-abstractions/pagination';
 import del from 'granite/mixins/controller-abstractions/delete';
 
 export default Controller.extend(pagination, del, {
+  application: controller(),
+  addingUser:  computed.equal('application.currentPath', 'account.anatomy.departments.index.new'),
   queryParams: [ 'page' ],
   limit:       10,
 
