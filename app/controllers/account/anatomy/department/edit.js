6afd776504cacef60784d5b7b4d49ba43265e3f1
petersonbrock <petersonbrock2@gmail.com>
2018-10-19 16:22:08 -0600

    added header and cancel button to editing department page

--- app/controllers/account/anatomy/department/edit.js
+++ app/controllers/account/anatomy/department/edit.js
@@ -3,5 +3,12 @@ import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 
 export default Controller.extend(addEdit, {
   transitionAfterSave: 'account.anatomy.departments.index',
-  transitionWithModel: false
+  transitionWithModel: false,
+
+  actions: {
+    cancel () {
+      this.get('model').rollbackAttributes();
+      this.transitionToRoute('account.anatomy.departments.index');
+    }
+  }
 });
