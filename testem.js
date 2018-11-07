ba278d9bbf97614301c83c4e7158f70e4e25351b
James-Mac <james@aehr.org>
2018-11-07 15:14:21 -0700

    Try browserstack

--- testem.js
+++ testem.js
@@ -8,12 +8,8 @@ module.exports = {
   browser_start_timeout: 120,
 
   launch_in_ci: [
-    'SL_MicrosoftEdge_public_16_16299_Windows_10',
-    'SL_chrome_public_56_0_Windows_10',
-    'SL_chrome_public_beta_Windows_10',
-    'SL_chrome_public_65_0_Windows_10',
-    'SL_firefox_public_59_0_Windows_10',
-    'SL_safari_public_11_0_macOS_10_13'
+    'bs_edge16',
+    'bs_chromelatest'
   ],
 
   launch_in_dev: [
@@ -37,118 +33,128 @@ module.exports = {
   },
 
   launchers: {
-    SL_MicrosoftEdge_public_16_16299_Windows_10: {
-      'exe': 'ember',
-      'args': [
-        'sauce:launch',
-        '-b',
-        'MicrosoftEdge',
-        '-v',
-        '16.16299',
-        '--visibility',
-        'public',
-        '-p',
-        'Windows 10',
-        '--attach',
-        '--no-connect',
-        '--url'
-      ],
-      'protocol': 'browser'
+    bs_edge16: {
+      exe: "npx browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "edge", "--bv", "16.0", "--u",
+      protocol: "browser"
     },
-
-    SL_chrome_public_56_0_Windows_10: {
-      'exe': 'ember',
-      'args': [
-        'sauce:launch',
-        '-b',
-        'chrome',
-        '-v',
-        '56.0',
-        '--visibility',
-        'public',
-        '-p',
-        'Windows 10',
-        '--attach',
-        '--no-connect',
-        '--url'
-      ],
-      'protocol': 'browser'
-    },
-
-    SL_chrome_public_beta_Windows_10: {
-      'exe': 'ember',
-      'args': [
-        'sauce:launch',
-        '-b',
-        'chrome',
-        '-v',
-        'beta',
-        '--visibility',
-        'public',
-        '-p',
-        'Windows 10',
-        '--attach',
-        '--no-connect',
-        '--url'
-      ],
-      'protocol': 'browser'
-    },
-
-    SL_chrome_public_65_0_Windows_10: {
-      'exe': 'ember',
-      'args': [
-        'sauce:launch',
-        '-b',
-        'chrome',
-        '-v',
-        '65.0',
-        '--visibility',
-        'public',
-        '-p',
-        'Windows 10',
-        '--attach',
-        '--no-connect',
-        '--url'
-      ],
-      'protocol': 'browser'
-    },
-
-    SL_firefox_public_59_0_Windows_10: {
-      'exe': 'ember',
-      'args': [
-        'sauce:launch',
-        '-b',
-        'firefox',
-        '-v',
-        '59.0',
-        '--visibility',
-        'public',
-        '-p',
-        'Windows 10',
-        '--attach',
-        '--no-connect',
-        '--url'
-      ],
-      'protocol': 'browser'
-    },
-
-    SL_safari_public_11_0_macOS_10_13: {
-      'exe': 'ember',
-      'args': [
-        'sauce:launch',
-        '-b',
-        'safari',
-        '-v',
-        '11.1',
-        '--visibility',
-        'public',
-        '-p',
-        'macOS 10.13',
-        '--attach',
-        '--no-connect',
-        '--url'
-      ],
-      'protocol': 'browser'
+    bs_chromelatest: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "chrome", "--bv", "latest", "-t", "600", "--u"],
+      protocol: "browser"
     }
+    // SL_MicrosoftEdge_public_16_16299_Windows_10: {
+    //   'exe': 'ember',
+    //   'args': [
+    //     'sauce:launch',
+    //     '-b',
+    //     'MicrosoftEdge',
+    //     '-v',
+    //     '16.16299',
+    //     '--visibility',
+    //     'public',
+    //     '-p',
+    //     'Windows 10',
+    //     '--attach',
+    //     '--no-connect',
+    //     '--url'
+    //   ],
+    //   'protocol': 'browser'
+    // },
+    //
+    // SL_chrome_public_56_0_Windows_10: {
+    //   'exe': 'ember',
+    //   'args': [
+    //     'sauce:launch',
+    //     '-b',
+    //     'chrome',
+    //     '-v',
+    //     '56.0',
+    //     '--visibility',
+    //     'public',
+    //     '-p',
+    //     'Windows 10',
+    //     '--attach',
+    //     '--no-connect',
+    //     '--url'
+    //   ],
+    //   'protocol': 'browser'
+    // },
+    //
+    // SL_chrome_public_beta_Windows_10: {
+    //   'exe': 'ember',
+    //   'args': [
+    //     'sauce:launch',
+    //     '-b',
+    //     'chrome',
+    //     '-v',
+    //     'beta',
+    //     '--visibility',
+    //     'public',
+    //     '-p',
+    //     'Windows 10',
+    //     '--attach',
+    //     '--no-connect',
+    //     '--url'
+    //   ],
+    //   'protocol': 'browser'
+    // },
+    //
+    // SL_chrome_public_65_0_Windows_10: {
+    //   'exe': 'ember',
+    //   'args': [
+    //     'sauce:launch',
+    //     '-b',
+    //     'chrome',
+    //     '-v',
+    //     '65.0',
+    //     '--visibility',
+    //     'public',
+    //     '-p',
+    //     'Windows 10',
+    //     '--attach',
+    //     '--no-connect',
+    //     '--url'
+    //   ],
+    //   'protocol': 'browser'
+    // },
+    //
+    // SL_firefox_public_59_0_Windows_10: {
+    //   'exe': 'ember',
+    //   'args': [
+    //     'sauce:launch',
+    //     '-b',
+    //     'firefox',
+    //     '-v',
+    //     '59.0',
+    //     '--visibility',
+    //     'public',
+    //     '-p',
+    //     'Windows 10',
+    //     '--attach',
+    //     '--no-connect',
+    //     '--url'
+    //   ],
+    //   'protocol': 'browser'
+    // },
+    //
+    // SL_safari_public_11_0_macOS_10_13: {
+    //   'exe': 'ember',
+    //   'args': [
+    //     'sauce:launch',
+    //     '-b',
+    //     'safari',
+    //     '-v',
+    //     '11.1',
+    //     '--visibility',
+    //     'public',
+    //     '-p',
+    //     'macOS 10.13',
+    //     '--attach',
+    //     '--no-connect',
+    //     '--url'
+    //   ],
+    //   'protocol': 'browser'
+    // }
   }
 };
