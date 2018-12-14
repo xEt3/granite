cc7850918aa83884053b2a8b02e07bf44bb983ce
petersonbrock <petersonbrock2@gmail.com>
2018-12-14 10:34:40 -0700

    added documents section to offboarding steps

---
+++ app/controllers/account/employee/offboard/documents.js
@@ -0,0 +1,62 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import Model from 'ember-data/model';
+import addEdit from 'granite/mixins/controller-abstractions/add-edit';
+
+export default Controller.extend(addEdit, {
+  enableNotify: false,
+
+  offboardingDocumentsFiltered: computed('offboardingDocuments', 'assignments.[]', function () {
+    let assignments = this.get('assignments');
+
+    return this.get('offboardingDocuments').filter((file) =>
+      !assignments.find(assignment =>
+        assignment.belongsTo('file').id() === file.get('id')));
+  }),
+
+  actions: {
+    async addAssignment (files) {
+      const makeAssignment = (inputFile) => {
+        let file = inputFile;
+
+        if (!(file instanceof Model)) {
+          this.store.pushPayload('file', { file });
+          file = this.store.peekRecord('file', file.id);
+        }
+
+        let assignment = this.store.createRecord('file-assignment', {
+          file,
+          employee:          this.get('employee'),
+          visibleToEmployee: true
+        });
+
+        this.assignments.pushObject(assignment);
+
+        return this.saveModel(assignment);
+      };
+
+      let _files = Array.isArray(files) ? files : [ files ];
+
+      for (let i = 0; i < _files.length; i++) {
+        try {
+          await makeAssignment(_files[i]);
+        } catch (e) {
+          this.set('enableNotify', true);
+          this.ajaxError(e);
+          this.set('enableNotify', false);
+        }
+      }
+
+      this.set('docModalSelection', []);
+    },
+
+    saveAssignmentChanges (assignment) {
+      this.saveModel(assignment);
+    },
+
+    async removeAssignment (assignment) {
+      await assignment.destroyRecord();
+      this.assignments.removeObject(assignment);
+    }
+  }
+});
