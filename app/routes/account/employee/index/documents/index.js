70f0a7a53813cbbdcb0df3b45afaeb2d1e2b7b78
petersonbrock <petersonbrock2@gmail.com>
2018-12-17 15:59:07 -0700

    added document pages under employee. added list-item for documents under employee

---
+++ app/routes/account/employee/index/documents/index.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import resource from 'granite/mixins/route-abstractions/resource';
+
+export default Route.extend(resource, {
+  titleToken: 'Documents',
+  modelName:  'file-assignment',
+
+  mutateQuery (q) {
+    q.employee = this.modelFor('account.employee').get('id');
+  }
+
+  //HOOK UP FILTERING
+});
