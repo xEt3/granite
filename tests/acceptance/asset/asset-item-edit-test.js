47c044593a4d36c852325ab56a358a8fbfa39f8f
Jeremy Cherer <chererjeremy@gmail.com>
2019-01-31 08:57:58 -0700

    Allow asset stock attribute editing
    closes #458

--- tests/acceptance/asset/asset-item-edit-test.js
+++ tests/acceptance/asset/asset-item-edit-test.js
@@ -1,31 +1,30 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import authenticate from 'granite/tests/helpers/auth';
-import { visit, currentURL, click, find, findAll, settled } from '@ember/test-helpers';
+import { visit, currentURL, click, settled, fillIn } from '@ember/test-helpers';
 
-module('Acceptance | company assets', function (hooks) {
+module('Acceptance | edit company assets', function (hooks) {
   setupApplicationTest(hooks);
 
 
-  test('assets information', async function (assert) {
-    let myAssetCategory = server.create('asset', {
-      name:       'apple',
-      attributes: [ 'blue' ]
-    });
-
-    await server.createList('asset-item', 7);
+  test('editing an asset', async function (assert) {
+    let myAssetCategory = server.create('asset');
     await authenticate.call(this, server);
     await visit('/account/assets');
     await settled();
     await click('div.content .header');
 
-    assert.dom('i.info.icon').exists();
-    await click('i.info.icon');
-    assert.equal(currentURL(), `/account/asset/${myAssetCategory.id}/information`, 'on assets information page');
+    assert.equal(currentURL(), `/account/asset/${myAssetCategory.id}/stock`, 'on assets stock page');
 
-    await settled();
-    assert.dom('.content.clearfix span').hasText('blue');
+    await click('.plus.icon');
+    await fillIn('#asset-identifier', 'text');
+    await fillIn(`input[placeholder="${myAssetCategory.attributes} value"]`, 'header');
+    await click('.ui.huge.green.button');
+    await click('span > .edit.icon');
+    await fillIn(`input[placeholder="${myAssetCategory.attributes} value"]`, 'updated attribute');
+    await click('.ui.huge.green.button');
+    await click('.down.angle.icon');
+
+    assert.dom('.line-item-value > u').hasText('updated attribute');
   });
-
-
 });
