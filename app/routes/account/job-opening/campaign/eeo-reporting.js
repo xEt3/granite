3d27edba39258cbb0051e1bb36487d2aeabbb047
petersonbrock <petersonbrock2@gmail.com>
2019-02-08 11:08:11 -0700

    fix eeo table mobile alignment

--- app/routes/account/job-opening/campaign/eeo-reporting.js
+++ app/routes/account/job-opening/campaign/eeo-reporting.js
@@ -8,17 +8,6 @@ export default Route.extend({
   model () {
     const jobOpening = this.modelFor('account.job-opening');
 
-    // let fakeData = [
-    //   [ 'Type', 'All', 'Reviewed', 'Not Reviewed', 'Disqualified', 'Sample Stage 1', 'Sample Stage 2', 'Hired' ],
-    //   { section: 'Race/Gender EEO Data' },
-    //   [ 'Male', 5, 2, 3, 1, 0, 2, 1 ],
-    //   [ 'Female', 5, 2, 3, 1, 0, 2, 1 ],
-    //   [ 'White', 1, 2, 3, 4, 5, 6, 7 ],
-    //   { section: 'Section Two' },
-    //   [ 'A', 1, 2, 3, 4, 5, 6, 7 ]
-    // ];
-    //
-    // return fakeData;
     return this.ajax.request('/api/v1/job-applications/', {
       data: {
         $report:    'eeo',
