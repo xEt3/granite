c0badc8c53eb29ec28d7a68478b8f5c15888d2f4
petersonbrock <petersonbrock2@gmail.com>
2019-02-08 14:21:48 -0700

    undo rando push

--- app/routes/account/job-opening/campaign/eeo-reporting.js
+++ app/routes/account/job-opening/campaign/eeo-reporting.js
@@ -8,7 +8,6 @@ export default Route.extend({
   model () {
     const jobOpening = this.modelFor('account.job-opening');
 
-
     return this.ajax.request('/api/v1/job-applications/', {
       data: {
         $report:    'eeo',
