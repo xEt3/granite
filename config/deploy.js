ad2aedede136ff4e7da3b16a505a0d26b3e35f10
James <james@aehr.org>
2019-03-04 11:22:26 -0700

    :lipstick:

--- config/deploy.js
+++ config/deploy.js
@@ -2,10 +2,10 @@
 
 module.exports = function (environment) {
   let ENV = Object.assign({}, environment);
-
+  console.log('environment', environment);
   ENV['revision-data'] = { type: 'file-hash' };
   ENV.rollbar = {
-    accessToken:       ENV.emberRollbarClient.accessToken,
+    accessToken:       '6f016ff3c5ef495b9c894871857aefd4',
     serverAccessToken: process.env.ROLLBAR_SERVER_TOKEN,
     minifiedPrependUrl (context) {
       return 'https://www.granitehr.com/' + context.revisionData.revisionKey + '/';
