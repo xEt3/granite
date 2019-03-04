59132b8cafd9d20e6123e38a269ffb25470c9f60
James <james@aehr.org>
2019-03-04 12:09:01 -0700

    Remove SCM generator

--- config/deploy.js
+++ config/deploy.js
@@ -3,7 +3,10 @@
 module.exports = function (/* targetEnv */) {
   let ENV = {};
 
-  ENV['revision-data'] = { type: 'file-hash' };
+  ENV['revision-data'] = {
+    type: 'version-commit',
+    scm:  false
+  };
   ENV.rollbar = {
     accessToken:       '6f016ff3c5ef495b9c894871857aefd4',
     accessServerToken: process.env.ROLLBAR_SERVER_TOKEN,
