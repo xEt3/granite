8895c02dd075c5b2d7775bccb9d562621ef1d481
James <james@aehr.org>
2019-03-04 11:27:23 -0700

    Remove bower, fix config keys

--- config/deploy.js
+++ config/deploy.js
@@ -1,18 +1,18 @@
 'use strict';
 
-module.exports = function (environment) {
-  let ENV = Object.assign({}, environment);
-  console.log('environment', environment);
+module.exports = function (/* targetEnv */) {
+  let ENV = {};
+
   ENV['revision-data'] = { type: 'file-hash' };
   ENV.rollbar = {
     accessToken:       '6f016ff3c5ef495b9c894871857aefd4',
-    serverAccessToken: process.env.ROLLBAR_SERVER_TOKEN,
+    accessServerToken: process.env.ROLLBAR_SERVER_TOKEN,
     minifiedPrependUrl (context) {
       return 'https://www.granitehr.com/' + context.revisionData.revisionKey + '/';
     }
   };
 
-  console.log('Deploy environment is', ENV);
+  console.log('Deploy environment is', ENV); /* eslint-disable-line */
 
   return ENV;
 };
