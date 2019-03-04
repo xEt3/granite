5f3020c07c2331e1dbc84d83afa670176619abd3
James <james@aehr.org>
2019-03-04 14:39:00 -0700

    This should correct version tag

--- config/deploy.js
+++ config/deploy.js
@@ -7,12 +7,15 @@ module.exports = function (/* targetEnv */) {
     type: 'file-hash',
     scm:  false
   };
+
+  let packageJson = require('../package.json'),
+      gitHash = (process.env.SOURCE_VERSION || '').substr(0, 7);
+
   ENV.rollbar = {
-    accessToken:       '6f016ff3c5ef495b9c894871857aefd4',
-    accessServerToken: process.env.ROLLBAR_SERVER_TOKEN,
-    minifiedPrependUrl (/* context */) {
-      return 'https://www.granitehr.com/';
-    }
+    accessToken:        '6f016ff3c5ef495b9c894871857aefd4',
+    accessServerToken:  process.env.ROLLBAR_SERVER_TOKEN,
+    revisionKey:        `${packageJson.version}+${gitHash}`,
+    minifiedPrependUrl: 'https://www.granitehr.com/'
   };
 
   console.log('Deploy environment is', ENV); /* eslint-disable-line */
