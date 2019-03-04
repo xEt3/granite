63f048ca2d27a9cfb476850f373e4780dac0259e
James <james@aehr.org>
2019-03-04 08:29:53 -0700

    Attempting to configure sourcemaps

---
+++ config/deploy.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function (environment) {
+  let ENV = Object.assign({}, environment);
+
+  ENV['revision-data'] = { type: 'git-commit' };
+  ENV.rollbar = {
+    accessToken:       ENV.emberRollbarClient.accessToken,
+    serverAccessToken: process.env.ROLLBAR_SERVER_TOKEN,
+    minifiedPrependUrl (context) {
+      return 'https://www.granitehr.com/' + context.revisionData.revisionKey + '/';
+    }
+  };
+
+  console.log('Deploy environment is', ENV);
+
+  return ENV;
+};
