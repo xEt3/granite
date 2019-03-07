70769eae3cf3b55ec47272ea1d56590673d125aa
petersonbrock <petersonbrock2@gmail.com>
2019-03-07 08:43:12 -0700

    add title token, other pr changes

--- app/routes/account/help/topic.js
+++ app/routes/account/help/topic.js
@@ -2,6 +2,10 @@ import Route from '@ember/routing/route';
 import { articles } from 'granite/config/help';
 
 export default Route.extend({
+  titleToken (model) {
+    return model.title;
+  },
+
   model (params) {
     return articles[params.topic_slug];
   }
