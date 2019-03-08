5f72b34953b7de10333fec5233587adc6626ba4d
Jeremy Cherer <chererjeremy@gmail.com>
2019-03-08 12:58:09 -0700

    Add listing url link to recruiting campaigns
    closes #511

--- app/components/list-item/job-opening-item.js
+++ app/components/list-item/job-opening-item.js
@@ -1,3 +1,12 @@
 import BaseLiComponent from './base';
+import { computed } from '@ember/object';
 
-export default BaseLiComponent.extend({ classNames: [ 'content' ] });
+
+export default BaseLiComponent.extend({
+  classNames: [ 'content' ],
+
+  quartzLink: computed('model.title', function () {
+    let title = this.model.title;
+    return title.replace(/\s/, '-') + '_' +  this.model.id;
+  })
+});
