c573d62bacf0f468548979a531bbc9605031f3a8
James <james@aehr.org>
2019-03-18 14:26:11 -0600

    Beef up description tags

--- app/routes/our-product.js
+++ app/routes/our-product.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default Route.extend({ titleToken: 'About Our Product' });
+export default Route.extend({
+  headData:   service(),
+  titleToken: 'About Our Product',
+
+  afterModel () {
+    this.set('headData.description', 'Learn more about how Granite HR impowers your company with powerful human resource management tools. Asset management, employee self-service, & recruiting are just a few things we offer.');
+  }
+});
--- app/routes/pricing.js
+++ app/routes/pricing.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default Route.extend({ titleToken: 'Pricing' });
+export default Route.extend({
+  headData:   service(),
+  titleToken: 'Pricing',
+
+  afterModel () {
+    this.set('headData.description', 'Simplistic and upfront pricing, backed with a 14-day trial gives you utter confidence in billing. Granite HR is a lightweight HRIS that helps you conquer human resources.');
+  }
+});
