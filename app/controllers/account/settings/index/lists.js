d024cebc38882a84d955773d8eef92e79e9c4621
petersonbrock <petersonbrock2@gmail.com>
2019-04-19 14:25:52 -0600

    add edit functionality to modal

--- app/controllers/account/settings/index/lists.js
+++ app/controllers/account/settings/index/lists.js
@@ -4,32 +4,73 @@ import $ from 'jquery';
 
 export default Controller.extend(addEdit, {
   queryParams: [ 'list' ],
-  tempList:    [ 1, 2, 3, 4 ],
-  list:        [],
+  list:        null,
   dirtyList:   false,
+  currentItem: null,
 
   actions: {
-    toggleList (list) {
-      this.set('list', list);
-    },
-
     async saveList () {
       let company = this.get('company');
       await this.saveModel(company);
       this.set('dirtyList', false);
     },
 
-    addItem (item) {
-      console.log('inside addItem');
+    toggleList (list) {
+      this.set('list', list);
     },
 
-    openModal (modal) {
-      console.log('inside openModal');
-      $('#settings__add-edit-list-item').modal('show');
+    openModal () {
+      this.set('respondedModal', false);
+
+      $('#settings__add-edit-list-item').modal({
+        detachable: true,
+        context:    '.ember-application',
+        onHidden:   () => {
+          this.set('currentItem', null);
+          if (!this.get('respondedModal')) {
+            this.send('respondModal', false);
+          }
+        }
+      }).modal('show');
+
+      return new Promise((resolve, reject) => this.setProperties({
+        resolve,
+        reject
+      }));
     },
 
     closeModal () {
       $('#settings__add-edit-list-item').modal('hide');
+    },
+
+    respondModal (response) {
+      this.get(response ? 'resolve' : 'reject')(response);
+      this.set('respondedModal', true);
+      this.send('closeModal');
+    },
+
+    beginEdit (item, index) {
+      this.setProperties({
+        index,
+        currentItem: item,
+        editingItem: true
+      });
+    },
+
+    addItem () {
+      let currentItem = this.get('currentItem');
+
+      this.get('model').addObject(currentItem);
+      this.set('dirtyList', true);
+    },
+
+    editItem () {
+      let currentItem = this.get('currentItem'),
+          index = this.get('index');
+
+      this.get('model').removeAt(index);
+      this.get('model').insertAt(index, currentItem);
+      this.set('dirtyList', true);
     },
 
     deleteItem (item) {
