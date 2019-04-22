2c32cc597e54277efba4ba8d04512738e20ea800
petersonbrock <petersonbrock2@gmail.com>
2019-04-22 14:52:50 -0600

    tests for add/edit/deleting from lists

--- mirage/factories/company.js
+++ mirage/factories/company.js
@@ -21,6 +21,21 @@ export default Factory.extend({
     'anatomy'
   ],
 
+  disqualificationReasons: [
+    'Better qualified candidates selected -- education',
+    'Campaign on hold or eliminated',
+    'Not willing to work hours',
+    'Failed test',
+    'Failed background check',
+    'Company does not wish to sponsor work visa',
+    'Accepted offer at another company',
+    'Candidate applied to wrong position',
+    'Compensation unacceptable',
+    'Job does not meet career objectives',
+    'Not willing to relocate',
+    'No show to interview'
+  ],
+
   afterCreate (company, server) {
     server.createList('corrective-action-severity', 3, { company });
   }
