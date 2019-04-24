85ed665f44c46483d24bb81b18c19641f2e96c42
James <james@aehr.org>
2019-04-24 14:06:36 -0600

    Fix/add test for display (blank)

--- tests/acceptance/education/educate-employee-test.js
+++ tests/acceptance/education/educate-employee-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import authenticate from 'granite/tests/helpers/auth';
 // import { faker } from 'ember-cli-mirage';
-import { visit, currentURL, click, settled, find } from '@ember/test-helpers';
+import { visit, currentURL, click, settled, findAll } from '@ember/test-helpers';
 // import moment from 'moment';
 
 module('Acceptance | Educate | Employee Page', function (hooks) {
@@ -36,15 +36,13 @@ module('Acceptance | Educate | Employee Page', function (hooks) {
     assert.equal(currentURL(), `/account/employee/${employee.id}/education`);
 
     assert.dom('.ui.segment > hgroup > h1').includesText('Education');
-    await click('.ui.segment .menu__actions');
+    await click('.ui.segment .menu__actions #add-education');
     await settled();
-    window.f = find;
-    this.pauseTest();
 
-    assert.equal(find('.ui.segment .menu__actions .menu > a').length, 2, 'Has two menu items');
-    assert.dom('.ui.segment > .ui.list:first-child ~ h2').includesText('Certifications');
-    assert.dom('.ui.segment > .ui.list:last-child ~ h2').includesText('Training Assignments');
-    assert.dom('.ui.segment > .ui.list:first-child').includesText('No certifications');
-    assert.dom('.ui.segment > .ui.list:last-child').includesText('No training');
+    assert.equal(findAll('.ui.segment .menu__actions .menu > a').length, 2, 'Has two menu items');
+    assert.dom('.header__list:first-of-type').includesText('Certifications');
+    assert.dom('.header__list:last-of-type').includesText('Training Assignments');
+    assert.dom('.header__list:first-of-type + .ui.list').includesText('No certifications');
+    assert.dom('.header__list:last-of-type + .ui.list').includesText('No training');
   });
 });
