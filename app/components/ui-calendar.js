cf6701176aab044a5b6fdc1556d73490fd497f1c
petersonbrock <petersonbrock2@gmail.com>
2019-07-25 15:13:39 -0600

    design ui-calendar component.  switch edit employee index to fit component

--- app/components/ui-calendar.js
+++ app/components/ui-calendar.js
@@ -1,7 +1,17 @@
 import Component from '@ember/component';
 import Base from 'semantic-ui-ember/mixins/base';
+import { computed } from '@ember/object';
 
 export default Component.extend(Base, {
   module:     'calendar',
-  classNames: [ 'ui', 'calendar' ]
+  classNames: [ 'ui', 'calendar' ],
+
+  calendarId: computed('elementId', function () {
+    return `${this.get('elementId')}__calendar`;
+  }),
+
+  init () {
+    this._super(...arguments);
+    this.set('value', this.get('dateValue'));
+  }
 });
--- app/routes/account/employee/index/edit/index.js
+++ app/routes/account/employee/index/edit/index.js
@@ -1,12 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-  titleToken: 'Edit Personal',
-
-  setupController (controller, model) {
-    controller.setProperties({
-      model,
-      initialDateOfBirth: model.dateOfBirth
-    });
-  }
-});
+export default Route.extend({ titleToken: 'Edit Personal' });
