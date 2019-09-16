bc6d9ccb46975651ebb379a9a53111c077d4f3a0
Jeremy Cherer <chererjeremy@gmail.com>
2019-09-16 11:17:29 -0600

    On-boarding auto fill State to match company state
    closes #635

--- app/routes/account/employee/onboard/index.js
+++ app/routes/account/employee/onboard/index.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({ titleToken: 'Onboard' });
+export default Route.extend({
+  titleToken: 'Onboard',
+
+  async model () {
+    let employee = this._super(...arguments),
+        { addressState } = await employee.get('company');
+
+    employee.set('addressState', addressState);
+    return employee;
+  }
+});
