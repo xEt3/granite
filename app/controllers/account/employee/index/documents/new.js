6674ec87963cb06c77dceb5f63304fa0c104b3b8
Jeremy Cherer <chererjeremy@gmail.com>
2019-10-09 11:07:28 -0600

    adding to employee/document and "assigned by"

--- app/controllers/account/employee/index/documents/new.js
+++ app/controllers/account/employee/index/documents/new.js
@@ -4,6 +4,7 @@ import addEdit from 'granite/mixins/controller-abstractions/add-edit';
 export default Controller.extend(addEdit, {
   transitionAfterSave: 'account.employee.index.documents',
   transitionWithModel: false,
+  isExpanded:          false,
 
   afterSave () {
     this.send('refresh');
