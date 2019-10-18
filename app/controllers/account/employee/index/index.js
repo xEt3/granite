12eceeaf0c3164328a31eae640bf53be3df4f715
Jeremy Cherer <chererjeremy@gmail.com>
2019-10-18 11:45:13 -0600

    add a note section in offboarding for internal use only
    closes #668

--- app/controllers/account/employee/index/index.js
+++ app/controllers/account/employee/index/index.js
@@ -12,11 +12,19 @@ export default Controller.extend(addEdit, {
     return cf ? Object.keys(cf).length : 0;
   }),
 
+  cliffNotes: computed('separationNotes', function () {
+    return this.get('model.separationNotes').match(/([\w]+\s+|.+){1,6}/)[0].trim();
+  }),
+
   hasAnniversaries:    computed.or('model.{hireDate,dateOfBirth}'),
   hasDirectContact:    computed.or('model.{phone,email}'),
   hasEmergencyContact: computed.or(
     'model.{emergencyContactPhone,emergencyContactNameFirst,emergencyContactNameLast}'
   ),
+
+  terminated: computed('model.terminatedOn', function () {
+    return moment(this.get('model.terminatedOn')).format('MM/DD/YY');
+  }),
 
   onProbation: computed('model.probationUntil', function () {
     return this.get('model.probationUntil') && moment(this.get('model.probationUntil')).isAfter(moment());
