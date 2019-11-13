b65b87cea73b52e9150f7df6827df24e17d666b6
Jeremy Cherer <chererjeremy@gmail.com>
2019-11-13 08:01:50 -0700

    pr fix

--- app/components/list-item/document-assignment.js
+++ app/components/list-item/document-assignment.js
@@ -1,4 +1,16 @@
 import BaseLiComponent from './base';
 import { or } from '@ember/object/computed';
 
-export default BaseLiComponent.extend({ isPendingState: or('model.isLoading', 'model.isSaving', 'model.isReloading') });
+export default BaseLiComponent.extend({
+  isPendingState: or('model.isLoading', 'model.isSaving', 'model.isReloading'),
+
+  actions: {
+    checkBoxMagic () {
+      let model = this.get('model');
+
+      model.set('signatureRequired', model.visibleToEmployee ? true : false);
+      model.set('effectiveOn', model.visibleToEmployee ? model.effectiveOn : null);
+      return;
+    }
+  }
+});
