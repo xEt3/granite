115d18652495ca5518a4438cce4966d5ab67ab42
Jeremy Cherer <chererjeremy@gmail.com>
2019-12-30 09:25:37 -0700

    🛠,  🧹, and   💄
    fixing, cleaning, and making it look better

--- app/controllers/account/settings/user.js
+++ app/controllers/account/settings/user.js
@@ -5,46 +5,32 @@ import modalSupport from 'granite/mixins/modal-support';
 import ajaxStatus from 'granite/mixins/ajax-status';
 
 export default Controller.extend(modalSupport, ajaxStatus, {
-  ajax: service(),
+  ajax:      service(),
+  passwords: {},
 
   actions: {
     async change () {
-      let userId = this.get('auth.session.user');
-      let current = this.get('current-password');
-      let newPassword = this.get('new-password');
-      let confirmedPassword = this.get('confirm-password');
+      let { passwords } = this;
 
       this.ajaxStart();
 
-      console.log('tis', this);
-
-      if (current === newPassword) {
-        this.ajaxError('New and Old Passwords can\'t match');
+      if (passwords.newPassword !== passwords.confirmPassword) {
+        this.ajaxError('Passwords do not match.');
         return;
       }
 
-      if (newPassword === confirmedPassword) {
-        try {
-          await this.get('ajax').post('/api/v1/company-user/change-password', {
-            data: {
-              newPassword,
-              current,
-              userId
-            }
-          });
-          this.set('current-password', null);
-          this.set('new-password', null);
-          this.set('confirmed-password', null);
-          this.ajaxSuccess('Successfully Changed.');
-
-        } catch (e) {
-          this.ajaxError(e);
-        }
-
+      try {
+        await this.get('ajax').post('/api/v1/company-user/change-password', {
+          data: {
+            passwords,
+            userId: this.get('auth.userId')
+          }
+        });
+        this.set('passwords', {});
+        this.ajaxSuccess('Successfully Changed.');
+      } catch (e) {
+        this.ajaxError(e);
       }
-
-
-
     }
   }
 });
