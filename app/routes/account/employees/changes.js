fa6b04d614a20450b3acafa81a92671f0ad0c806
Jeremy Cherer <chererjeremy@gmail.com>
2020-02-10 14:33:50 -0700

    pr fix
    showing current value if its not the same as before

--- app/routes/account/employees/changes.js
+++ app/routes/account/employees/changes.js
@@ -1,14 +1,18 @@
 import Route from '@ember/routing/route';
+import resource from 'granite/mixins/route-abstractions/resource';
+import { inject as service } from '@ember/service';
 
-export default Route.extend({
+export default Route.extend(resource, {
   titleToken: 'Changes',
+  auth:       service(),
+  modelName:  'change',
 
-  model () {
-    return this.get('store').query('change', {
-      reviewedOn: { $not: { $type: 9 } },
-      sort:       { created: -1 }
-    });
+  query: {
+    '$report':  'changeList',
+    reviewedOn: { $not: { $type: 9 } }
   },
+
+  sort: { created: -1 },
 
   actions: {
     refresh () {
