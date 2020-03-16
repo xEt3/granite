3d0d644ae2a5d22fe3eb584430791b0f805f41eb
Jeremy Cherer <chererjeremy@gmail.com>
2020-03-16 11:35:49 -0600

    Employee sub-page menu doesn't show "Summary" on mobile
    closes #743

--- app/controllers/account/employee/index.js
+++ app/controllers/account/employee/index.js
@@ -8,8 +8,26 @@ export default Controller.extend(del, {
   ajax:                service(),
   application:         controller(),
   transitionAfterSave: 'account.employees',
+  icons:               {
+    equipment:  'mobile',
+    changes:    'history',
+    documents:  'file',
+    counseling: 'folder open',
+    education:  'graduation cap'
+  },
 
-  onSummary: computed.equal('application.currentPath', 'account.employee.index.index'),
+  onSummary:   computed.equal('application.currentPath', 'account.employee.index.index'),
+  currentPath: computed.reads('application.currentPath'),
+
+  subRoute: computed('target.currentURL', function () {
+    let path = this.target.currentURL.split('/');
+    let currentMenu = path[path.length - 1];
+    return currentMenu === 'history' ? 'changes' : currentMenu;
+  }),
+
+  classMenu: computed('subRoute', function () {
+    return this.icons[this.get('subRoute')];
+  }),
 
   actions: {
     resend () {
