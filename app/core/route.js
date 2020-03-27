e5535e3c740ab36c2e5f47681ca2a822100fbef2
James <james@aehr.org>
2020-03-27 14:30:32 -0600

    Abstracting, converting an "add" route

--- app/core/route.js
+++ app/core/route.js
@@ -1,7 +1,33 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
+import { route } from './abstractions';
 
 export default class GraniteRoute extends Route {
+  model () {
+    const method = this.__methodForRouteType('model');
+
+    if (method) {
+      return method.apply(this, arguments);
+    }
+
+    super.model(...arguments);
+  }
+
+  __methodForRouteType (methodName) {
+    return this.routeType && route[this.routeType][methodName];
+  }
+
+  @action
+  willTransition () {
+    const method = this.__methodForRouteType('willTransition');
+
+    if (method) {
+      return method.apply(this, arguments);
+    }
+
+    super.willTransition(...arguments);
+  }
+
   @action
   refreshModel () {
     this.refresh();
