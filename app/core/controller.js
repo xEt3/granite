329c1c508eda967ee8f878c5797c4d68983965f7
James <james@aehr.org>
2020-04-01 15:19:01 -0600

    Goodbye resource mixin

--- app/core/controller.js
+++ app/core/controller.js
@@ -1,4 +1,12 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default class GraniteController extends Controller {
+  @service auth
+}
+
+export class GraniteResourceController extends GraniteController {
+  queryParams = [ 'page', 'limit' ]
+  limit       = 20
+  page        = 1
 }
