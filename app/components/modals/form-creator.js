511743e277cc89c6b4b86efdc8a6a83e159e0477
petersonbrock <petersonbrock2@gmail.com>
2020-04-02 17:24:56 -0600

    more work on converting wizards and related components

--- app/components/modals/form-creator.js
+++ app/components/modals/form-creator.js
@@ -1,94 +1,94 @@
 /*
   USAGE
 
-  {{#modals/form-creator
-    formType="exit-interview"
-    onCreateForm=(action "setForm") as |openFormModal|
-  }}
-    <a href="#" {{action openFormModal}}>
+  <Modals::FormCreator
+    @formType="exit-interview"
+    @onCreateForm={{this.setForm}} as |openFormModal|
+  >
+    <a href="#" {{on "click" (prevent-default (fn openFormModal null))}}>
       Create form
     </a>
-  {{/modals/form-creator}}
+  </Modals::FormCreator>
 */
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import addEdit from 'granite/mixins/controller-abstractions/add-edit';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import $ from 'jquery';
 
-export default Component.extend(addEdit, {
-  auth:  service(),
-  store: service(),
-
-  tagName: [ 'span' ],
+export default class ModalsFormCreatorComponent extends Component {
+  @service data
+  @service auth
+  @service store
+  @tracked form
 
   afterSave () {
-    let form = this.get('form'),
+    let form = this.form,
         removeDuplicates = [];
 
-    form.get('elements').forEach(e => {
+    form.elements.forEach(e => {
       if (!e.get('id')) {
         e.destroy();
         removeDuplicates.push(e);
       }
     });
-    form.get('elements').removeObjects(removeDuplicates);
-  },
+    form.elements.removeObjects(removeDuplicates);
+  }
 
-  sortGroupClass: computed('showingPreview', function () {
-    return `ui middle aligned divided list form-creator__form-elements ${this.get('showingPreview') ? 'form-creator__form-elements--preview' : ''}`;
-  }),
+  get sortGroupClass () {
+    return `ui middle aligned divided list form-creator__form-elements ${this.showingPreview ? 'form-creator__form-elements--preview' : ''}`;
+  }
 
-  openFormModal: computed('modalId', function () {
-    return this.openModal.bind(this);
-  }),
+  get modalId () {
+    return `modal__form-creator-${this.elementId}`;
+  }
 
-  modalId: computed('elementId', function () {
-    return `modal__form-creator-${this.get('elementId')}`;
-  }),
+  @action
+  openFormModal (existingForm) {
+    const { store, args: { formType } } = this;
 
-  openModal (existingForm) {
-    const { formType, store } = this;
-
-    this.set('form', existingForm || store.createRecord('form', {
+    this.form = existingForm || store.createRecord('form', {
       formType,
       name: `${formType.charAt(0).toUpperCase()}${formType.replace(/-/g, ' ').slice(1)} form`
-    }));
+    });
 
-    $(`#${this.get('modalId')}`).modal({
+    $(`#${this.modalId}`).modal({
       detachable: true,
       closable:   false,
       context:    '.ember-application'
     }).modal('show');
-  },
+  }
 
+  @action
   closeModal () {
     if (this.form.isNew) {
       this.form.deleteRecord();
-      this.set('form', null);
+      this.form = null;
     }
 
-    $(`#${this.get('modalId')}`).modal('hide');
-  },
+    $(`#${this.modalId}`).modal('hide');
+  }
 
-  actions: {
-    notify (type, msg) {
-      if (this.onNotify) {
-        this.onNotify(type, msg);
-      }
-    },
-
-    addFormElement () {
-      let formElement = this.store.createRecord('form-element', {});
-      this.get('form.elements').pushObject(formElement);
-    },
-
-    deleteFormElement (element) {
-      this.get('form.elements').removeObject(element);
-    },
-
-    reorderElements (elements) {
-      this.get('form').set('elements', elements);
+  @action
+  notify (type, msg) {
+    if (this.onNotify) {
+      this.data.notify(type, msg);
     }
   }
-});
+
+  @action
+  addFormElement () {
+    let formElement = this.store.createRecord('form-element', {});
+    this.form.elements.pushObject(formElement);
+  }
+
+  @action
+  deleteFormElement (element) {
+    this.form.elements.removeObject(element);
+  }
+
+  @action
+  reorderElements (elements) {
+    this.form.elements = elements;
+  }
+}
--- app/services/data.js
+++ app/services/data.js
@@ -249,7 +249,7 @@ export default class DataService extends Service {
       setTimeout(() => {
         if (!this.get('isDestroyed') && !this.get('isDestroying')) {
           this.set(`statuses.${label}`, {
-            isLoading: true,
+            isLoading: false,
             isLoaded:  true,
             isSlow:    false,
             message:   null,
