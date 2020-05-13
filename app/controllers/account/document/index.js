0b9c3ce4b2de6575d250502e853887368a47b055
James <james@aehr.org>
2020-05-13 15:42:53 -0600

    Working through tests

--- app/controllers/account/document/index.js
+++ app/controllers/account/document/index.js
@@ -1,6 +1,8 @@
 import Controller from 'granite/core/controller';
+import { tracked } from '@glimmer/tracking';
 import { match } from '@ember/object/computed';
 
 export default class AccountDocumentController extends Controller {
   @match('model.extension', /je?pg|png|gif/i) imagePreview
+  @tracked fileAssignments
 }
