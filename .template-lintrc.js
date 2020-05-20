aab94f7580f2aa4e6f143a1736ab8ce89a3f2998
James <james@aehr.org>
2020-05-20 14:14:09 -0600

    Cleaning lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,8 +4,11 @@ module.exports = {
   extends: 'octane',
   rules:   {
     'self-closing-void-elements':    false,
-    'no-curly-component-invocation': { requireDash: true },
-    'no-implicit-this':              { allow: [ '_' ] }
+    'no-curly-component-invocation': {
+      requireDash: true,
+      allow:       [ 'relative-greeting-time', 'app-version', 'liquid-outlet' ]
+    },
+    'no-implicit-this': { allow: [ '_', 'relative-greeting-time', 'app-version' ] }
   },
   ignore: [
     'addon/**'
