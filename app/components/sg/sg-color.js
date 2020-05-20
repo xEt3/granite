5aabe55770c906303a3c63345d7ea01311ab91c1
James <james@aehr.org>
2020-05-20 14:55:34 -0600

    Fixing more things

--- app/components/sg/sg-color.js
+++ app/components/sg/sg-color.js
@@ -1,7 +1,9 @@
+import classic from 'ember-classic-decorator';
 import { classNames, classNameBindings } from '@ember-decorators/component';
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 
+@classic
 @classNames('sg__color', 'two wide column')
 @classNameBindings('computedClassName')
 export default class SgColor extends Component {
