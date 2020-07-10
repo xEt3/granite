0f3a3e85954e8eaba805af8efab2412b1321cf37
Jeremy Cherer <chererjeremy@gmail.com>
2020-07-10 10:54:08 -0600

    added title tokens

--- app/routes/account/benefits.js
+++ app/routes/account/benefits.js
@@ -1,3 +1,7 @@
 import Route from 'granite/core/route';
 
-export default class BenefitsRoute extends Route {}
+export default class BenefitsRoute extends Route {
+  titleToken () {
+    return 'Benefits'
+  }
+}
