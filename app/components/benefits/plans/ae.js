6e8722e28797378213837fc202b28d9c6f8bd4f6
Jeremy Cherer <chererjeremy@gmail.com>
2020-07-14 10:36:01 -0600

    added link for API

--- app/components/benefits/plans/ae.js
+++ app/components/benefits/plans/ae.js
@@ -1,4 +1,21 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class AeComponent extends Component {}
+export default class AeComponent extends Component {
+
+  @action
+  async linkCarrier () {
+    try {
+      await this.ajax.post('/api/v1/benefits/plan-download', {
+        carrierData: {
+          authorizationCode: this.authorizationCode,
+          companyID:         this.companyId
+        }
+      });
+    } catch (e) {
+      error(e);
+      throw e;
+    }
+  }
+}
 
