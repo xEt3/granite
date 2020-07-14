bd00909a9dfc50b040dc333efe37cc8c3aab27a2
Jeremy Cherer <chererjeremy@gmail.com>
2020-07-14 09:16:00 -0600

    cleaning up

--- app/routes/account/benefits/carrier-link.js
+++ app/routes/account/benefits/carrier-link.js
@@ -2,16 +2,12 @@ import Route from '@ember/routing/route';
 import { carriers } from 'granite/config';
 
 export default Route.extend({
-  async model(params){
+ async model(params){
     if (!params){
       return
     }
+    let carrier = await carriers.filter(carrier => carrier.key === params.carrier_key)
 
-    return await carriers.reduce(carrier => {
-        if (carrier.key === params.carrier_key){
-          return carrier
-        }
-        return
-      })
+    return carrier[0]
     }
 });
--- app/routes/account/benefits/plans.js
+++ app/routes/account/benefits/plans.js
@@ -1,7 +1,6 @@
 import Route from 'granite/core/route';
 import { carriers } from 'granite/config';
 
-
 export default class PlansRoute extends Route {
   titleToken = 'Plans'
     async model () {
