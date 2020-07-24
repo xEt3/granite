a7414e4aea3cb4c4f81de6363324aa3604554596
James <james@aehr.org>
2020-07-24 07:56:28 -0600

    Working on benefits

--- app/routes/account/benefits/carrier-link.js
+++ app/routes/account/benefits/carrier-link.js
@@ -1,5 +1,5 @@
 import Route from 'granite/core/route';
 
 export default class CarrierLinkRoute extends Route {
- titleToken='Carriers'
+ titleToken = 'Carriers'
 }
---
+++ app/routes/account/benefits/plans/index.js
@@ -0,0 +1,26 @@
+import Route from 'granite/core/route';
+import { inject as service } from '@ember/service';
+
+export default class IntegrationsRoute extends Route {
+  @service auth;
+
+  async model () {
+    return this.store.query('plan', { company: (await this.auth.user).get('company.id') });
+  }
+
+  setupController (controller, plans) {
+    controller.setProperties({
+      plans,
+      groupedPlans: plans && plans.reduce((group, plan) => {
+        const { label } = plan;
+
+        if (!group[label]) {
+          group[label] = [];
+        }
+
+        group[plan.label].push(plan);
+        return group;
+      }, {})
+    });
+  }
+}
