f2904ef886df0e530abb8480432ca274a962ba1a
Jeremy Cherer <chererjeremy@gmail.com>
2020-07-24 10:02:29 -0600

    Open enrollment model
    closes #772

---
+++ app/models/open-enrollment.js
@@ -0,0 +1,10 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+
+export default class OpenEnrollment extends Model {
+  @belongsTo('company') company
+  @attr('number') month
+  @attr('number') day
+  @attr('date', { defaultValue: () => new Date() }) created;
+}
