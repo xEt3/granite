1aa751a246308d64400fc7cf4321f2d9b2e9e030
James <james@aehr.org>
2020-09-14 14:39:04 -0600

    Update app/components/benefits/manual-plan.js

--- app/components/benefits/manual-plan.js
+++ app/components/benefits/manual-plan.js
@@ -3,7 +3,7 @@ import { planForm, planOptions } from '../../config/forms/plans';
 import { inject as service } from '@ember/service';
 
 export default class BenefitsManualPlanComponent extends Component {
-currentForm = planForm
-planOptions = planOptions
-@service data
+  currentForm = planForm
+  planOptions = planOptions
+  @service data
 }
