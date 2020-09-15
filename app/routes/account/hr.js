3892bb53c048e0a7fc2a1ab4e765641f356cbae6
James <james@aehr.org>
2020-09-15 10:44:57 -0600

    Polish up design and add wolters kluwer

--- app/routes/account/hr.js
+++ app/routes/account/hr.js
@@ -1,5 +1,12 @@
 import Route from 'granite/core/route';
 
 export default class HrRoute extends Route {
-  titleToken = 'HR Resources';
+  titleToken = 'HR Resources'
+
+  async model () {
+    const user = await this.auth.user,
+          company = await user.company;
+
+    return company.email;
+  }
 }
