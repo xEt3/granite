fb0cf1fb7d326970bb3953c3f758b6a8422d6a3e
James <james@aehr.org>
2020-09-21 06:12:15 -0600

    Update app/components/elements/company-navigation.js

--- app/components/elements/company-navigation.js
+++ app/components/elements/company-navigation.js
@@ -5,7 +5,7 @@ import { htmlSafe } from '@ember/string';
 import $ from 'jquery';
 
 export default class CompanyNavigationComponent extends Component {
-  @service() auth
+  @service auth
 
   get logoUrl () {
     let img = this.auth.get('user.company.logoUrl') || '/assets/images/granite-logo-mountains.png';
