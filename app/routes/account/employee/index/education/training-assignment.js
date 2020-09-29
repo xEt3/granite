66213b31cef87bc1c6a4dc096a77f53f9f032b07
Jeremy Cherer <chererjeremy@gmail.com>
2020-09-29 11:05:23 -0600

    Able to delete training assignments
    closes #824

---
+++ app/routes/account/employee/index/education/training-assignment.js
@@ -0,0 +1,12 @@
+import Route from 'granite/core/route';
+
+export default class TrainingAssignmentRoute extends Route {
+  titleToken (model) {
+    return model.name;
+  }
+
+  async model ({ assignment_id }) {
+    let w = await this.store.find('TrainingAssignment', assignment_id);
+    return w;
+  }
+}
---
+++ app/routes/account/employee/index/education/training-assignment/index.js
@@ -0,0 +1,3 @@
+import Route from 'granite/core/route';
+
+export default class AccountEmployeeEducationTrainingAssignmentRoute extends Route {}
