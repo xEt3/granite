44b2b7ea662c1ca05b9125fa45dd4c80c8867977
petersonbrock <petersonbrock2@gmail.com>
2020-11-18 10:13:15 -0700

    💄

--- app/controllers/account/settings/index/features.js
+++ app/controllers/account/settings/index/features.js
@@ -43,7 +43,6 @@ export default class AccountSettingsFeaturesController extends Controller {
       this.model.benefitNotification.addObjects(externalEmails);
     }
 
-
     this.dirtyNotificationList();
   }
 
